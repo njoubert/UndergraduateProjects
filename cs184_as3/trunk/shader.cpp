c694b2d158b890c67a5cd30b67f3964a9abb0abc
njoubert <njoubert@997b1fe7-5dcb-cc6a-e049-b7da040a1fbe>
2008-02-13 01:20:22 +0000

    Initial stuff to get project going. no real code yet!
    git-svn-id: svn+ssh://njoubert.com/var/svn@9 997b1fe7-5dcb-cc6a-e049-b7da040a1fbe

---
+++ cs184_as3/trunk/shader.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : CS184_as3.cpp
+// Author      : Niels Joubert
+// Version     :
+// Copyright   : 2008
+// Description : Hello World in C, Ansi-style
+//============================================================================
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("!!!Hello World!!!");
+	return EXIT_SUCCESS;
+}
