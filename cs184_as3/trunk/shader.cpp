6a64e18fc64144af86848c5f8cfb4775cad760f8
njoubert <njoubert@997b1fe7-5dcb-cc6a-e049-b7da040a1fbe>
2008-02-18 09:47:32 +0000

    Basic diffuse and ambient shading works. Coordinate system is screwed up, vectors don;'t normalize right and its weird but we're getting somewhere
    git-svn-id: svn+ssh://njoubert.com/var/svn@21 997b1fe7-5dcb-cc6a-e049-b7da040a1fbe

--- cs184_as3/trunk/shader.cpp
+++ cs184_as3/trunk/shader.cpp
@@ -11,6 +11,7 @@
 #include <iostream>
 #include <fstream>
 #include <cmath>
+#include <limits.h>
 //#include <stdlib>
 
 #ifdef _WIN32
@@ -41,68 +42,174 @@ using namespace std;
 // Some Classes
 //****************************************************
 
-class Viewport;
 class Color;
+class Position3d;
 class Vector3d;
 class Light;
 class PointLight;
 class DirectionalLight;
 class Object3d;
 
-class Viewport {
-public:
-	int w, h; // width and height
-};
-
 class Color {
 public:
-	float r, g, b;	
-	Color(float r, float g, float b) { r = 0; g = 0; b = 0; }
+	
+	Color() {}
+	Color(float r, float g, float b) { setColor(r,g,b); }
+	
+	void setColor(float r, float g, float b) { this->r = r; this->g = g; this->b = b; }
+	float getNormalizedR(float m) { return r / m; }
+	float getNormalizedG(float m) { return g / m; }
+	float getNormalizedB(float m) { return b / m; }
+	
 	Color & operator*=(const Color& other) {
 		r *= other.r;
 		g *= other.g;
 		b *= other.b;
 		return *this;
 	}
-
+	Color & operator*=(const Color* other) {
+		r *= other->r;
+		g *= other->g;
+		b *= other->b;
+		return *this;
+	}
+	Color & operator *=(float v) {
+		r *= v;
+		g *= v;
+		b *= v;
+		return *this;
+	}	
 	Color operator *(const Color& other) const {
 		Color result = *this; //Make a copy of the current class
 		result *= other;
 		return result;
 	}
+	Color operator *(const Color * other) const {
+		Color result = *this; //Make a copy of the current class
+		result *= other;
+		return result;
+	}
+	Color operator +=(const Color& other) {
+		r += other.r;
+		g += other.g;
+		b += other.b;
+		return *this;
+	}
+	
+	Color operator +(const Color& other) const {
+		Color result = *this;
+		result += other;
+		return result;
+	}
+	
+	void debugMe() { printDebug("(" << r << "," << g << "," << b << ")"); }
+	
+	float getMaxVal() {
+		return max(max(r,g),b);
+	}
+	
+	void normalizeTo(float m) {
+		if (m == 0)
+			m = 1;
+		//printDebug("Normalizing color by dividing with " << m);
+		r = r / m;
+		g = g / m;
+		b = b / m;
+	}
+	
+private:
+	float r, g, b;	
+};
+
+class Position3d {
+public:
+	Position3d() { x = 0; y = 0; z = 0; }
+	float getX() { return x; }
+	float getY() { return y; }
+	float getZ() { return z; }
+	void setX(float x) { this->x = x; }
+	void setY(float y) { this->y = y; }
+	void setZ(float z) { this->z = z; }	
+	void setPos(float x, float y, float z) {
+		setX(x); setY(y); setZ(z);
+	}
+	void debugMe() { printDebug("(" << x << "," << y << "," << z << ")"); }
+private:	
+	float x, y, z; // width and height
 };
 
 class Vector3d {
 public:
+	
+	Vector3d() {}
+	
+	void calculateFromPositions(Position3d * start, Position3d * end) {
+		setX(end->getX() - start->getX());
+		setY(end->getY() - start->getY());
+		setZ(end->getZ() - start->getZ());
+	}
+	
+	float dot(Vector3d * other) {
+		return x*other->x + y*other->y + z*other->z;
+	}
+	
+	Vector3d normalize() {
+		Vector3d result = *this; //Make a copy of the current class
+		float l = sqrt(x*x + y*y + z*z);
+		result.x = result.x/l;
+		result.y = result.y/l;
+		result.z = result.z/l;
+		return result;
+	}
+	Vector3d normalize(float length) {
+		Vector3d result = *this; //Make a copy of the current class
+		result.x = result.x/length;
+		result.y = result.y/length;
+		result.z = result.z/length;
+		return result;
+	}
+	void debugMe() { printDebug("(" << x << "," << y << "," << z << ")"); }
+	
+	float getX() { return (int) x; }
+	float getY() { return (int) y; }
+	float getZ() { return (int) z; }
+	void setX(float x) { this->x = x; }
+	void setY(float y) { this->y = y; }
+	void setZ(float z) { this->z = z; }		
+private:	
 	float x, y, z; // width and height
 };
 
 class Light {
 public:
 	Light() {
-		
+		printDebug("Creating a light...");
 	}
 	//This creates a light with position (x,y,z) and color (r,g,b)
 	//x, y, z are relative to sphere's center (0 is center, 1 is radius)
 	//r, g, b are positive floats between [0.0, max_float]
 	Light(float x, float y, float z, float r, float g, float b) {
-		printDebug("Object3d::setAmbientColor(" << r << "," << g << "," << b << ");");
+		printDebug("Creating Light at " << "(" << x << "," << y << "," << z << ") with color (" << r << "," << g << "," << b << ");");
+		setPosition(x,y,z);
+		setLuminosity(r,g,b);
 	}
 	void setPosition(float x, float y, float z) {
-		
+		P_relPosition.setX(x);
+		P_relPosition.setY(y);
+		P_relPosition.setZ(z);
 	}
-	void setPosition(Vector3d v) {
-		
+	Position3d * getPosition() {
+		return &P_relPosition;
 	}
 	void setLuminosity(float r, float g, float b) {
-		
+		C_lum.setColor(r,g,b);	
 	}
-	void setLuminosity(Vector3d v) {
-		
+	Color * getLuminosity() {
+		return &C_lum;
 	}
 private:
-	Vector3d V_position;
-	Vector3d V_luminosity;
+	Position3d P_relPosition;
+	Color C_lum;
 };
 
 class PointLight: public Light {
@@ -122,30 +229,24 @@ public:
 	void setAmbientColor(float r, float g, float b) {
 		//Assume r g b values are [0.0,1.0]
 		printDebug("Object3d::setAmbientColor(" << r << "," << g << "," << b << ");");
-		C_ambient.r = r;
-		C_ambient.g = g;
-		C_ambient.b = b;
+		C_ambient.setColor(r,g,b);
 	}
 	void setDiffuseColor(float r, float g, float b) {
 		//Assume r g b values are [0.0,1.0]
 		printDebug("Object3d.setDiffuseColor(" << r << "," << g << "," << b << ");");
-		C_diffuse.r = r;
-		C_diffuse.g = g;
-		C_diffuse.b = b;
+		C_diffuse.setColor(r,g,b);
 	}
 	void setSpecularColor(float r, float g, float b) {
 		//Assume r g b values are [0.0,1.0]
 		printDebug("Object3d.setSpecularColor(" << r << "," << g << "," << b << ");");
-		C_specular.r = r;
-		C_specular.g = g;
-		C_specular.b = b;
+		C_specular.setColor(r,g,b);
 	}
 	void setSpecularCoeff(float arg) {
 		printDebug("Object3d.setSpecular(" << arg << ");");
 		v = arg;
 	}
-	virtual void render(vector<Light*> lights) = 0;
-private:
+	virtual void render(vector<Light*> & lights, Vector3d & view) = 0;
+protected:
 	float v;
 	Color C_ambient;
 	Color C_diffuse;
@@ -156,44 +257,107 @@ class Sphere: public Object3d {
 public:
 	Sphere() {
 		radius = 100;
+		center.setPos(0,0,0);
 	}
 	~Sphere() {}
 	
-	void render(vector<Light*> lights) {
+	void setRadius(float x) {
+		radius = x;
+	}
+	
+	void render(vector<Light*> & lights, Vector3d & view) {
+		Position3d point;
+		Vector3d normal;
+		Vector3d incidence;
+		Vector3d reflectance;
+		Color C_pixel;
+		Color C_tempDiff;
+		Light* l;
 		
+		printDebug("Shading a sphere...");
+		
+		glBegin(GL_POINTS);
+			
+		for (float i = -radius; i <= radius; i += 1) {
+			float width = (sqrt((float)(radius*radius-i*i)) - 0.5f);
+			for (float j = -width; j <= width; j += 1) {
+				
+		/*
+
+1) compute the xyz location of the point on the sphere.  Since you know the xy location of the pixel and radius of the sphere, you can figure out the z coordinate.  Call this location "p".  Compute the normal for the point also, "n".
+
+2) if the pixel is outside the radius of the sphere set it black on move on
+
+3) set the initial color for the pixel to ka
+
+4) for each light
+
+	4a) compute l = l_pos_i-p
+	4b) v = [0,0,1]
+	4c) r = reflect(v,n) (see text)
+	4c) c = kd * l_i * ( n . l ) + ks * l_i * (r.l)^q
+	4d) pixel color += c
+
+5) draw pixel
+				
+*/				
+				C_pixel = C_ambient;
+				
+				point.setPos(center.getX() + i, center.getY() + j,0);
+				calculateZ(point);
+				
+				normal.calculateFromPositions(&center,&point);
+				normal = normal.normalize(radius);
+				
+				for (unsigned int i = 0; i < lights.size(); i++) {
+						
+					l = lights[i];
+					incidence.calculateFromPositions(l->getPosition(),&point);
+					incidence.normalize();
+					C_tempDiff = C_diffuse * ((l->getLuminosity()));
+					C_tempDiff *= max(0.0f, incidence.dot(&normal));
+					C_pixel += C_tempDiff;
+					
+				}
+				
+				C_pixel.normalizeTo(C_pixel.getMaxVal());
+				setPixel(point.getX(), point.getY(), C_pixel.getNormalizedR(1), C_pixel.getNormalizedG(1), C_pixel.getNormalizedB(1));
+				
+				
+				
+			}
+		}
+		
+		glEnd();
 	}
 private:
 	float radius;
+	Position3d center;
+	
+	//Uses the x and y coordinates to calculate z on a sphere.
+	void calculateZ(Position3d & pos) {
+		//Use pythoagoras' theorem in 3D:
+		float x = pos.getX();
+		float y = pos.getY();
+		pos.setZ(sqrt(radius*radius - x*x - y*y));
+	}
+	
+	void setPixel(float x, float y, GLfloat r, GLfloat g, GLfloat b) {
+		glColor3f(r, g, b);
+		glVertex2f(x, y);
+	}
+	
 };
 
 
 //****************************************************
 // Global Variables
 //****************************************************
-Viewport	viewport;
-int 		plotX = 400;
-int 		plotY = 300;
-Color bgColor(0,0,0);
+Color 		bgColor(0,0,0);
+Vector3d	viewport;
+vector<Light*> lights(0);
+Sphere		sphr;
 
-void setPixel(int x, int y, GLfloat r, GLfloat g, GLfloat b) {
-	glColor3f(r, g, b);
-	glVertex2f(x, y);
-}
-
-void circle(int x, int y, int radius) {
-	glBegin(GL_POINTS);
-
-	for (int i = -radius; i <= radius; i++) {
-		int width = (int)(sqrt((float)(radius*radius-i*i)) + 0.5f);
-		for (int j = -width; j <= width; j++) {
-
-			setPixel(x + j, y + i, 1.0f, 1.0f, 1.0f);
-		}
-	}
-	
-	glEnd();
-
-}
 
 //========================================
 //
@@ -201,16 +365,23 @@ void circle(int x, int y, int radius) {
 //
 //========================================
 
+
+
 /*
- * Initializes Viewport
+ * Callback function for window reshape event
  */
-void initScene(){
-	glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
-	glViewport (0,0,viewport.w,viewport.h);
+void myReshape(int w, int h) {
+	viewport.setX(w);
+	viewport.setY(h);
+	
+	glViewport (0,0,(int)viewport.getX(),(int)viewport.getY());
 	glMatrixMode(GL_PROJECTION);
 	glLoadIdentity();
-	gluOrtho2D(0,viewport.w, 0, viewport.h);
+	gluOrtho2D(-1*viewport.getX()/2,viewport.getX()/2,-1*viewport.getY()/2,viewport.getY()/2);
+	//gluOrtho2D(-1*viewport.x*100,viewport.x*100, -1*viewport.y*100, viewport.y*100);
+
 }
+
 
 /*
  * Callback function to draw scene 
@@ -220,8 +391,12 @@ void myDisplay() {
 	glClear(GL_COLOR_BUFFER_BIT);
 	glMatrixMode(GL_MODELVIEW);
 	glLoadIdentity();
-
-	//circle(plotX, plotY, min(viewport.w, viewport.h) / 4);
+	
+	Vector3d view;
+	view.setX(0);
+	view.setY(0);
+	view.setZ(1);
+	sphr.render(lights, view);
 
 	glFlush();
 	glutSwapBuffers();
@@ -229,26 +404,33 @@ void myDisplay() {
 }
 
 /*
- * Callback function for window reshape event
+ * Initializes Viewport
  */
-void myReshape(int w, int h) {
-	viewport.w = w;
-	viewport.h = h;
-
-	glViewport (0,0,viewport.w,viewport.h);
-	glMatrixMode(GL_PROJECTION);
-	glLoadIdentity();
-	gluOrtho2D(0, viewport.w, 0, viewport.h);
-
+void initScene(){
+	glClearColor(bgColor.getNormalizedR(1), bgColor.getNormalizedG(1), bgColor.getNormalizedB(1), 0.0f);
+	myReshape((int)viewport.getX(),(int)viewport.getY());
 }
+
+
+void processNormalKeys(unsigned char key, int x, int y) {
+	printDebug("Key pressed: " << key);
+	if (key == 32) 
+		exit(0);
+}
+
+//========================================
+//
+// Setup and Teardown Follows
+//
+//========================================
 
 int parseCommandLine(int argc, char *argv[], vector<Light*> & lights, Object3d * obj) {
 	
 	bool malformedArg;
 	bool printUsage = false;
 	float r=0, g=0, b=0, x=0, y=0, z=0, v=0;
-	
-	for (int i = 1; i < argc; i++) {
+	int i;
+	for (i = 1; i < argc; i++) {
 		
 			malformedArg = false;
 		
@@ -332,9 +514,10 @@ int parseCommandLine(int argc, char *argv[], vector<Light*> & lights, Object3d *
 			} else if (!strcmp(argv[i], "-bg")) {
 				
 				if (isThereMore(i, argc, 3)) {
-					bgColor.r = atof(argv[++i]);
-					bgColor.g = atof(argv[++i]);
-					bgColor.b = atof(argv[++i]);
+					r = atof(argv[++i]);
+					g = atof(argv[++i]);
+					b = atof(argv[++i]);
+					bgColor.setColor(r,g,b);
 				} else {
 					malformedArg = true;
 				}
@@ -366,10 +549,13 @@ void printUsage() {
 
 int main(int argc, char *argv[]) {
 
-	
-	vector<Light*> lights(10);
-	Sphere sphr;
 	Object3d *obj = &sphr;
+		
+	//Set some default values. changes as the person drags the window around
+	viewport.setX(800);
+	viewport.setY(600);
+	viewport.setZ(0);
+	sphr.setRadius(150);
 	
 	if (parseCommandLine(argc, argv, lights, obj)) {
 		printUsage();
@@ -377,10 +563,8 @@ int main(int argc, char *argv[]) {
 	}
 	
 	glutInit(&argc, argv);
-	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
-	viewport.w = 800;
-	viewport.h = 600;
-	glutInitWindowSize(viewport.w, viewport.h);
+	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+	glutInitWindowSize((int)viewport.getX(),(int)viewport.getY());
 	glutInitWindowPosition(0,0);
 	glutCreateWindow(argv[0]);
 
@@ -388,7 +572,7 @@ int main(int argc, char *argv[]) {
 
 	glutDisplayFunc(myDisplay);					// function to run when its time to draw something
 	glutReshapeFunc(myReshape);					// function to run when the window gets resized
-	//glutIdleFunc(myFrameMove); 		
+	glutKeyboardFunc(processNormalKeys);	
 	glutMainLoop();							// infinite loop that will keep drawing and resizing and whatever else
 
 	//cleanUp();
