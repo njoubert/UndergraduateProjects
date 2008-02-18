65a4d41db2704cde168a1ab338e5b6006f5b96ed
njoubert <njoubert@997b1fe7-5dcb-cc6a-e049-b7da040a1fbe>
2008-02-18 05:07:01 +0000

    Basic classes and inheritance defined. Should work right.
    git-svn-id: svn+ssh://njoubert.com/var/svn@20 997b1fe7-5dcb-cc6a-e049-b7da040a1fbe

--- cs184_as3/trunk/shader.cpp
+++ cs184_as3/trunk/shader.cpp
@@ -56,7 +56,8 @@ public:
 
 class Color {
 public:
-	int r, g, b;	
+	float r, g, b;	
+	Color(float r, float g, float b) { r = 0; g = 0; b = 0; }
 	Color & operator*=(const Color& other) {
 		r *= other.r;
 		g *= other.g;
@@ -84,7 +85,7 @@ public:
 	//This creates a light with position (x,y,z) and color (r,g,b)
 	//x, y, z are relative to sphere's center (0 is center, 1 is radius)
 	//r, g, b are positive floats between [0.0, max_float]
-	Light(float x, float y, float z, float r, float g, float b) : position(), luminosity() {
+	Light(float x, float y, float z, float r, float g, float b) {
 		printDebug("Object3d::setAmbientColor(" << r << "," << g << "," << b << ");");
 	}
 	void setPosition(float x, float y, float z) {
@@ -100,8 +101,8 @@ public:
 		
 	}
 private:
-	Vector3d position;
-	Vector3d luminosity;
+	Vector3d V_position;
+	Vector3d V_luminosity;
 };
 
 class PointLight: public Light {
@@ -116,28 +117,53 @@ public:
 
 class Object3d {
 public:
-	
+	Object3d() : C_ambient(0,0,0), C_diffuse(0,0,0), C_specular(0,0,0) {}
+	virtual ~Object3d() {}
 	void setAmbientColor(float r, float g, float b) {
 		//Assume r g b values are [0.0,1.0]
 		printDebug("Object3d::setAmbientColor(" << r << "," << g << "," << b << ");");
+		C_ambient.r = r;
+		C_ambient.g = g;
+		C_ambient.b = b;
 	}
 	void setDiffuseColor(float r, float g, float b) {
 		//Assume r g b values are [0.0,1.0]
 		printDebug("Object3d.setDiffuseColor(" << r << "," << g << "," << b << ");");
-		
+		C_diffuse.r = r;
+		C_diffuse.g = g;
+		C_diffuse.b = b;
 	}
 	void setSpecularColor(float r, float g, float b) {
 		//Assume r g b values are [0.0,1.0]
 		printDebug("Object3d.setSpecularColor(" << r << "," << g << "," << b << ");");
-		
+		C_specular.r = r;
+		C_specular.g = g;
+		C_specular.b = b;
 	}
 	void setSpecularCoeff(float arg) {
 		printDebug("Object3d.setSpecular(" << arg << ");");
 		v = arg;
 	}
+	virtual void render(vector<Light*> lights) = 0;
 private:
 	float v;
+	Color C_ambient;
+	Color C_diffuse;
+	Color C_specular;
+};
+
+class Sphere: public Object3d {
+public:
+	Sphere() {
+		radius = 100;
+	}
+	~Sphere() {}
 	
+	void render(vector<Light*> lights) {
+		
+	}
+private:
+	float radius;
 };
 
 
@@ -147,8 +173,7 @@ private:
 Viewport	viewport;
 int 		plotX = 400;
 int 		plotY = 300;
-
-
+Color bgColor(0,0,0);
 
 void setPixel(int x, int y, GLfloat r, GLfloat g, GLfloat b) {
 	glColor3f(r, g, b);
@@ -217,14 +242,10 @@ void myReshape(int w, int h) {
 
 }
 
-int parseCommandLine(int argc, char *argv[], vector<Light*> & lights, Object3d & obj) {
-	/* 
-	Light * l = new Light();
-	l->setLuminosity(1.0, 1.0, 1.0);
-	l->setPosition(50, 50, 0);
-	lights[0] = l; 
-	*/
+int parseCommandLine(int argc, char *argv[], vector<Light*> & lights, Object3d * obj) {
+	
 	bool malformedArg;
+	bool printUsage = false;
 	float r=0, g=0, b=0, x=0, y=0, z=0, v=0;
 	
 	for (int i = 1; i < argc; i++) {
@@ -241,7 +262,7 @@ int parseCommandLine(int argc, char *argv[], vector<Light*> & lights, Object3d &
 					r = atof(argv[++i]);
 					g = atof(argv[++i]);
 					b = atof(argv[++i]);
-					obj.setAmbientColor(r,g,b);
+					obj->setAmbientColor(r,g,b);
 				} else {
 					malformedArg = true;
 				}
@@ -253,7 +274,7 @@ int parseCommandLine(int argc, char *argv[], vector<Light*> & lights, Object3d &
 					r = atof(argv[++i]);
 					g = atof(argv[++i]);
 					b = atof(argv[++i]);
-					obj.setDiffuseColor(r,g,b);
+					obj->setDiffuseColor(r,g,b);
 				} else {
 					malformedArg = true;
 				}
@@ -265,7 +286,7 @@ int parseCommandLine(int argc, char *argv[], vector<Light*> & lights, Object3d &
 					r = atof(argv[++i]);
 					g = atof(argv[++i]);
 					b = atof(argv[++i]);
-					obj.setSpecularColor(r,g,b);
+					obj->setSpecularColor(r,g,b);
 				} else {
 					malformedArg = true;
 				}
@@ -275,7 +296,7 @@ int parseCommandLine(int argc, char *argv[], vector<Light*> & lights, Object3d &
 				
 				if (isThereMore(i, argc, 1)) {
 					v = atof(argv[++i]);
-					obj.setSpecularCoeff(v);
+					obj->setSpecularCoeff(v);
 				} else {
 					malformedArg = true;
 				}
@@ -308,15 +329,29 @@ int parseCommandLine(int argc, char *argv[], vector<Light*> & lights, Object3d &
 					malformedArg = true;
 				}
 				
+			} else if (!strcmp(argv[i], "-bg")) {
+				
+				if (isThereMore(i, argc, 3)) {
+					bgColor.r = atof(argv[++i]);
+					bgColor.g = atof(argv[++i]);
+					bgColor.b = atof(argv[++i]);
+				} else {
+					malformedArg = true;
+				}
+				
 			} else {
 				malformedArg = true;
 			}
 			
-			if (malformedArg)
+			if (malformedArg) {
 				printDebug("Malformed input arg in parsing command \"" << argv[i] << "\"");
+				printUsage = true;
+			}
 
 		
 	}
+	if (printUsage)
+		return 1;
 	return 0;
 	
 }
@@ -325,13 +360,16 @@ void printUsage() {
 	cout << "Usage: "<< endl;
 	cout << "  shader -ka r g b -kd r g b -ks r g b -sp v \\" << endl;
 	cout << "      [[-pl x y z r g b] ... [-pl x y z r g b]] \\" << endl;
-	cout << "      [[-dl x y z r g b] ... [-dl x y z r g b]" << endl;
+	cout << "      [[-dl x y z r g b] ... [-dl x y z r g b] \\" << endl;
+	cout << "      [-bg r g b]" << endl;
 }
+
 int main(int argc, char *argv[]) {
 
 	
 	vector<Light*> lights(10);
-	Object3d obj;
+	Sphere sphr;
+	Object3d *obj = &sphr;
 	
 	if (parseCommandLine(argc, argv, lights, obj)) {
 		printUsage();
