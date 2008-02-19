8dc1737de7e6b3f60be06a604d70477499a8fd62
njoubert <njoubert@997b1fe7-5dcb-cc6a-e049-b7da040a1fbe>
2008-02-19 19:17:11 +0000

    git-svn-id: svn+ssh://njoubert.com/var/svn@38 997b1fe7-5dcb-cc6a-e049-b7da040a1fbe

--- cs184_site/trunk/public_html/assignments.php
+++ cs184_site/trunk/public_html/assignments.php
@@ -9,17 +9,17 @@
 	</td>
         <td class="body_content">
 
-<h1>Class Assignments</h1>
+<h2>Class Assignments</h2>
 
 <p>
-<h2>Assignment 1</h2>
+<h3>Assignment 1</h3>
 <br>This is purely some example code, here to help us get our system set up for OpenGL work. I used simple OpenGL polygon drawing routines to draw a little mohawked robot. Here's the result:
 <br><img src="images/as1.jpg" />
 <br><a href="http://www.wetherobots.com">We The Robots</a> inspired this creation.
 </p>
 
 <p>
-	<h2>Assignment 2</h2>
+	<h3>Assignment 2</h3>
 	This assignment examines simple shading models - specifically, the Phone Illuminiation Model.
 	Source Codde will be posted after the due date. For now, enjoy the pretty pictures!
 	
