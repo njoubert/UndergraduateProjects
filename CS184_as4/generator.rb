0448d7b3d85134d4841ee46754d9a7cd79d9bc66
njoubert <njoubert@997b1fe7-5dcb-cc6a-e049-b7da040a1fbe>
2008-03-21 21:02:21 +0000

    git-svn-id: svn+ssh://njoubert.com/var/svn@95 997b1fe7-5dcb-cc6a-e049-b7da040a1fbe

--- CS184_as4/generator.rb
+++ CS184_as4/generator.rb
@@ -1,23 +1,27 @@
 #!/usr/bin/ruby
 
-stepsize = 5
+stepsize = 12
+r = 2
+limithigh = 20
+limitlow = -20
 
 File.open("out.scene", "w") do |f|
 
-	f.puts "eye 0.0 0.0 5.0" 
-	f.puts "viewport -5.0 -5.0 -1.0 5.0 -5.0 -1.0 5.0 5.0 -1.0 -5.0 5.0 -1.0"
+	f.puts "eye 0.0 0.0 #{limithigh*4}" 
+	f.puts "viewport -8.0 -8.0 #{limithigh*(3)} 8.0 -8.0 #{limithigh*(3)} 8.0 8.0 #{limithigh*(3)} -8.0 8.0 #{limithigh*(3)}"
 	
-	f.puts "pointlight 0 0 20 0.8 0.8 0.8"
+	f.puts "pointlight 50 50 50 0.7 0.6 0.5"
+	f.puts "pointlight -50 -50 50 0.5 0.6 0.7"
 	
-	x = -10
-	while x <= 10
-		y = -10
-		while y <= 10
-			z = -20
-			while z <= -10
+	x = limitlow
+	while x <= limithigh
+		y = limitlow
+		while y <= limithigh
+			z = limitlow
+			while z <= limithigh
 			
 				#puts "#{x} #{y} #{z}"
-				f.puts "sphere \t#{x} #{y} #{z} 1.25 0.1 0.1 0.1 20.0 0.3 0.3 0.3 0.3 0.3 0.3 1.0 1.0 1.0"
+				f.puts "sphere \t#{x} #{y} #{z} #{r} 1.0 1.0 1.0 60.0 0.15 0.15 0.15 #{x.abs.to_f/20} #{y.abs.to_f/20} #{z.abs.to_f/20} 1.0 1.0 1.0"
 				
 				z += stepsize
 			end
@@ -25,6 +29,8 @@ File.open("out.scene", "w") do |f|
 		end
 		x += stepsize
 	end
+	
+	#f.puts "triangle -20 -20 20 20 -20 20 0 0 -20 1.0 1.0 1.0 60.0 0.15 0.15 0.15 0.4 0.4 0.4 1.0 1.0 1.0"
 	
 
 end
