3b744a9064847e5087254d90715b1dc92cc36bc1
njoubert <njoubert@997b1fe7-5dcb-cc6a-e049-b7da040a1fbe>
2008-04-09 09:25:37 +0000

    git-svn-id: svn+ssh://njoubert.com/var/svn@155 997b1fe7-5dcb-cc6a-e049-b7da040a1fbe

--- cs184_site/trunk/public_html/as5.php
+++ cs184_site/trunk/public_html/as5.php
@@ -13,12 +13,12 @@
 
 <p><h3>Drawing Bezier Curves</h3>
 <br>To get a feel for Bezier curves, I put together a little Processing app that draws Bezier curves. Drag the control points around to change the shape of the curve. Feel free to check out the source code.
-<br><iframe src="as5/Bezier/index.html" width=420 height=550 frameborder=0 scrolling="auto"></iframe>
+<br><iframe src="as5/Bezier/index.html" width=420 height=550 frameborder=0 scrolling="no"></iframe>
 
 </p>
 <p><h3>Calculating Bezier Surface points</h3>
 <br>Since I could now calculate points on a curve, it was a logical step to attempt finding points on a surface. This example draws the Teapot example surface in a point-based rendering methodology by finding points on surfaces and plotting them. This is purely to experiment with reading in the bez file, and calculating points (and derivates) across the surface.
-<br><iframe src="as5/BezierSurface/index.html" width=420 height=450 frameborder=0 scrolling="auto"></iframe>
+<br><iframe src="as5/BezierSurface/index.html" width=420 height=500 frameborder=0 scrolling="no"></iframe>
 </p>
 
 
