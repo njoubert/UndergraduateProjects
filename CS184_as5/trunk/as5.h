4ba52b8d80741134306bb362b117adbd9298a3bd
njoubert <njoubert@997b1fe7-5dcb-cc6a-e049-b7da040a1fbe>
2008-04-12 22:33:19 +0000

    git-svn-id: svn+ssh://njoubert.com/var/svn@172 997b1fe7-5dcb-cc6a-e049-b7da040a1fbe

---
+++ CS184_as5/trunk/as5.h
@@ -0,0 +1,38 @@
+#ifndef AS5_H_
+#define AS5_H_
+
+// Simple OpenGL example for CS184 sp08 by Trevor Standley, modified from sample code for CS184 on Sp06
+#include <vector>
+#include <iostream>
+#include <fstream>
+#include <cmath>
+
+#ifdef _WIN32
+#	include <windows.h>
+#else
+#	include <sys/time.h>
+#endif
+
+#ifdef OSX
+#include <GLUT/glut.h>
+#include <OpenGL/glu.h>
+#else
+#include "GL/glut.h"
+#include <GL/glu.h>
+#endif
+
+#include <time.h>
+#include <math.h>
+
+#ifdef _WIN32
+static DWORD lastTime;
+#else
+static struct timeval lastTime;
+#endif
+
+#define PI 3.14159265
+
+//****** Add our headers ******
+#include "bezier.h"
+
+#endif /*AS5_H_*/
--- CS184_as5/trunk/bezier.h
+++ CS184_as5/trunk/bezier.h
@@ -1,7 +1,6 @@
 	#ifndef BEZIER_H_
 #define BEZIER_H_
 
-#include "glut-3.7.6-bin/GL/glut.h"
 #include "cml/cml.h"
 typedef glVertex3d Point;
 typedef glNormal3d Normal;
