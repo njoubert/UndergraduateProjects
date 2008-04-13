7e96f08bb143f03a01688bd30f143852fa4d428b
njoubert <njoubert@997b1fe7-5dcb-cc6a-e049-b7da040a1fbe>
2008-04-13 00:48:20 +0000

    Types
    git-svn-id: svn+ssh://njoubert.com/var/svn@185 997b1fe7-5dcb-cc6a-e049-b7da040a1fbe

---
+++ CS184_as5/trunk/types.h
@@ -0,0 +1,12 @@
+#ifndef TYPES_H_
+#define TYPES_H_
+
+//****** Add libraries ******
+#include "cml/cml.h"
+
+//****** Add Types ******
+typedef cml::vector3d Point;
+typedef cml::vector3d Normal;
+typedef cml::vector3d Deriv;
+
+#endif /*TYPES_H_*/
