bf945c64f91969e1ca3edf3cf3bff190bfe18f68
Crystile Schande <Crystile Schande@997b1fe7-5dcb-cc6a-e049-b7da040a1fbe>
2008-04-13 00:05:40 +0000

    new includes
    git-svn-id: svn+ssh://njoubert.com/var/svn@182 997b1fe7-5dcb-cc6a-e049-b7da040a1fbe

--- CS184_as5/trunk/bezier.h
+++ CS184_as5/trunk/bezier.h
@@ -1,7 +1,11 @@
-	#ifndef BEZIER_H_
+#ifndef BEZIER_H_
 #define BEZIER_H_
 
 #include "as5.h"
+#include "cml/cml.h"
+typedef cml::vector3d Point;
+typedef cml::vector3d Normal;
+typedef cml::vector3d Deriv;
 
 struct Bezier {
 	Point p;
