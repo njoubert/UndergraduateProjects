88523b6c83f3369292b95cc56e23c36039d26d38
njoubert <njoubert@997b1fe7-5dcb-cc6a-e049-b7da040a1fbe>
2008-04-18 08:27:32 +0000

    git-svn-id: svn+ssh://njoubert.com/var/svn@235 997b1fe7-5dcb-cc6a-e049-b7da040a1fbe

--- cs184_site/trunk/public_html/as5.php
+++ cs184_site/trunk/public_html/as5.php
@@ -35,16 +35,16 @@
 	<td>Flat Shading</td>
 </tr>
 <tr>
-	<td><img src="as5/Images/test_uniform_smooth.png"></td>
-	<td><img src="as5/Images/test_uniform_flat.png"></td>
+	<td><img src="as5/Images/test_uniform_smooth.png" width=400 height=400/></td>
+	<td><img src="as5/Images/test_uniform_flat.png" width=400 height=400/></td>
 </tr>
 <tr>
 	<td>Wireframe</td>
 	<td>Wireframe with black normals, green derivatives</td>
 </tr>
 <tr>
-	<td><img src="as5/Images/test_uniform_wireframe.png"></td>
-	<td><img src="as5/Images/test_uniform_wireframewithdn.png"></td>
+	<td><img src="as5/Images/test_uniform_wireframe.png" width=400 height=400/></td>
+	<td><img src="as5/Images/test_uniform_wireframewithdn.png" width=400 height=400/></td>
 </tr>
 </table>
 
@@ -62,16 +62,16 @@
 	<td>Flat Shading</td>
 </tr>
 <tr>
-	<td><img src="as5/Images/test_adaptive_smooth.png"></td>
-	<td><img src="as5/Images/test_adaptive_flat.png"></td>
+	<td><img src="as5/Images/test_adaptive_smooth.png" width=400 height=400/></td>
+	<td><img src="as5/Images/test_adaptive_flat.png" width=400 height=400/></td>
 </tr>
 <tr>
 	<td>Wireframe</td>
-	<td>Black normals, Green derivatives - Palm Tree Style!</td>
+	<td>Black normals, Green derivatives - Palm tree!</td>
 </tr>
 <tr>
-	<td><img src="as5/Images/test_adaptive_wireframe.png"></td>
-	<td><img src="as5/Images/test_adaptive_nd.png"></td>
+	<td><img src="as5/Images/test_adaptive_wireframe.png" width=400 height=400/></td>
+	<td><img src="as5/Images/test_adaptive_nd.png" width=400 height=400/></td>
 </tr>
 </table>
 
@@ -80,20 +80,49 @@
 <p><h3>Teapot! Uniform Subdivision</h3>
 <br/><code>./cs184_as5 ../teapot.bez 0.1</code>
 
-<br/><img src="as5/Images/teapot_uniform_smooth.png"/>
-<br/><img src="as5/Images/teapot_uniform_flat.png"/>
-<br/><img src="as5/Images/teapot_uniform_wireframe.png"/>
-<br/><img src="as5/Images/teapot_uniform_dn.png"/>
+<table>
+<tr>
+	<td>Smooth Shading</td>
+	<td>Flat Shading</td>
+</tr>
+<tr>
+	<td><img src="as5/Images/teapot_uniform_smooth.png" width=400 height=400/></td>
+	<td><img src="as5/Images/teapot_uniform_flat.png" width=400 height=400/></td>
+</tr>
+<tr>
+	<td>Wireframe</td>
+	<td>Wireframe, Black normals, Green derivatives - Prickly plant</td>
+</tr>
+<tr>
+	<td><img src="as5/Images/teapot_uniform_wireframe.png" width=400 height=400/></td>
+	<td><img src="as5/Images/teapot_uniform_dn.png" width=400 height=400/></td>
+</tr>
+</table>
+
 
 </p>
 
 <p><h3>Teapot! Adaptive Subdivision</h3>
 <br/><code>./cs184_as5 ../teapot.bez 0.01 -a</code>
 
-<br/><img src="as5/Images/teapot_adaptive_smooth.png"/>
-<br/><img src="as5/Images/teapot_adaptive_flat.png"/>
-<br/><img src="as5/Images/teapot_adaptive_wireframe.png"/>
-<br/><img src="as5/Images/teapot_adaptive_n.png"/>
+<table>
+<tr>
+	<td>Smooth Shading</td>
+	<td>Flat Shading</td>
+</tr>
+<tr>
+	<td><img src="as5/Images/teapot_adaptive_smooth.png" width=400 height=400/></td>
+	<td><img src="as5/Images/teapot_adaptive_flat.png" width=400 height=400/></td>
+</tr>
+<tr>
+	<td>Wireframe</td>
+	<td>Wireframe, Black normals, Green derivatives - Prickly plant</td>
+</tr>
+<tr>
+	<td><img src="as5/Images/teapot_adaptive_wireframe.png" width=400 height=400/></td>
+	<td><img src="as5/Images/teapot_adaptive_n.png" width=400 height=400/></td>
+</tr>
+</table>
 
 </p>
 
