276f7afe74effd3cfccf5e3d7ddf2378bf7ecc43
njoubert <njoubert@997b1fe7-5dcb-cc6a-e049-b7da040a1fbe>
2008-09-08 22:40:14 +0000

    bumpmapping removed. mac makefile.
    git-svn-id: svn+ssh://njoubert.com/var/svn@372 997b1fe7-5dcb-cc6a-e049-b7da040a1fbe

--- cs184_as3/trunk/CS184_as3/shader.cpp
+++ cs184_as3/trunk/CS184_as3/shader.cpp
@@ -11,14 +11,13 @@
 #include <iostream>
 #include <math.h>
 #include <cmath>
-#include <cstdlib> 
+#include <cstdlib>
 
 #include "Debug.cpp"
 #include "Color.cpp"
 #include "Vector3d.cpp"
 #include "Position.cpp"
 #include "Light.cpp"
-#include "BumpMap.cpp"
 
 #ifdef _WIN32
 #	include <windows.h>
@@ -47,8 +46,6 @@ using namespace std;
 // Some Classes
 //****************************************************
 
-BumpMap bmap;
-
 void setPixel(float x, float y, GLfloat r, GLfloat g, GLfloat b) {
 	glColor3f(r, g, b);
 	glVertex2f(x, y);
@@ -66,74 +63,74 @@ class Sphere {
 public:
 	Sphere() { specularCoeff=1; center.setPositionValues(0,0,0);}
 	~Sphere() { }
-	
+
 	void setAmbientColorValues(float r, float g, float b) { ambient.setColor(r,g,b);}
 	void setDiffuseColorValues(float r, float g, float b) { diffuse.setColor(r,g,b);}
 	void setSpecularColorValues(float r, float g, float b) { specular.setColor(r,g,b);}
 	void setSpecularCoeff(float f) {specularCoeff = f;}
-	void setAbsRadiusValue(int f) { 
-		printDebug("Sphere radius set to " << f << " absolute."); 
+	void setAbsRadiusValue(int f) {
+		printDebug("Sphere radius set to " << f << " absolute.");
 		this->radius = f;
 	}
 	void setCenter(float x, float y) {
 		center.x = x;
 		center.y = y;
 	}
-	
+
 	void render(vector<Light*> & lights, Vector3d & view) {
 		AbsPosition3d point;
 		Vector3d normal,incidence,reflectance;
 		Color point_ambient,point_diffuse,point_specular,point_total;
 		float x=0,y=0,z=0,width=0;
-		
+
 		printDebug("Rendering sphere of radius " << radius);
-		
+
 		glBegin(GL_POINTS);
-		
+
 		for (y = -radius; y <= radius; y += 0.25) {
 			width = sqrt(radius*radius - y*y);
 			for (x = -width; x <= width; x += 0.25) {
-				
+
 					z = sqrt(radius*radius - x*x - y*y);
 					point.setPositionValues(center.x+x,center.y+y,center.z+z);
 					normal.calculateFromPositions(&center,&point);
 					normal.normalize();
-					
+
 					if (BM) {
 						//normal = normal + U*horiz + V*vert
-						
+
 						//We need to find the two vectors that are tangent to
 						//the sphere at this point, and multiply them by
 						//the gradients to positon the texture correctly
-						normal.setX(normal.getX() + 
-								bmap.getHorizontalGradient((int)point.x, (int)point.y) + 
-								bmap.getVerticalGradient((int)point.x, (int)point.y));
-						normal.setY(normal.getY() + 
-								bmap.getHorizontalGradient((int)point.x, (int)point.y) + 
-								bmap.getVerticalGradient((int)point.x, (int)point.y));					
+//						normal.setX(normal.getX() +
+//								bmap.getHorizontalGradient((int)point.x, (int)point.y) +
+//								bmap.getVerticalGradient((int)point.x, (int)point.y));
+//						normal.setY(normal.getY() +
+//								bmap.getHorizontalGradient((int)point.x, (int)point.y) +
+//								bmap.getVerticalGradient((int)point.x, (int)point.y));
 					}
-						
-					
+
+
 					point_diffuse.setColor(0,0,0);
 					point_ambient = ambient;
 					point_specular.setColor(0,0,0);
-					
-					
+
+
 					for (unsigned int i = 0; i < lights.size(); i++) {
-						
+
 							incidence = lights[i]->getIncidence(point,radius);
 							incidence.normalize();
 							reflectance.calculateReflective(incidence,normal);
 							reflectance.normalize();
-							
-							point_diffuse += (diffuse * lights[i]->getColor()) * 
+
+							point_diffuse += (diffuse * lights[i]->getColor()) *
 											max(incidence.dot(&normal),0.0f);
-							point_specular += (specular * lights[i]->getColor()) * 
-											pow(max(reflectance.dot(&view),0.0f),specularCoeff); 
-						
+							point_specular += (specular * lights[i]->getColor()) *
+											pow(max(reflectance.dot(&view),0.0f),specularCoeff);
+
 					}
-					
-					point_total = point_ambient + point_diffuse + point_specular;			
+
+					point_total = point_ambient + point_diffuse + point_specular;
 					setPixel(point.x,
 							point.y,
 							point_total.getGlR(0,1),
@@ -144,11 +141,11 @@ public:
 		}
 
 		glEnd();
-		
+
 	}
-	
+
 private:
-	
+
 	Color ambient;
 	Color diffuse;
 	Color specular;
@@ -181,7 +178,7 @@ void myReshape(int w, int h) {
 	viewport.h = h;
 	printDebug("Reshaping viewport to " << w << "x" << h);
 	sphr.setAbsRadiusValue(min(w,h)/2 - min(w,h)/10);
-	
+
 	glViewport (0,0,viewport.w,viewport.h);
 	glMatrixMode(GL_PROJECTION);
 	glLoadIdentity();
@@ -189,17 +186,17 @@ void myReshape(int w, int h) {
 }
 
 /*
- * Callback function to draw scene 
+ * Callback function to draw scene
  */
 void myDisplay() {
 	printDebug("Drawing Scene");
 	glClear(GL_COLOR_BUFFER_BIT);
 	glMatrixMode(GL_MODELVIEW);
 	glLoadIdentity();
-	
+
 	Vector3d view(0,0,1);
 	sphr.render(lights, view);
-	
+
 	glFlush();
 	glutSwapBuffers();
 }
@@ -216,7 +213,7 @@ void initScene(){
 
 void processNormalKeys(unsigned char key, int x, int y) {
 	printDebug("Key pressed: " << key);
-	if (key == 32) 
+	if (key == 32)
 		exit(0);
 	if (key == 'f')
 		ANI += 2;
@@ -233,21 +230,21 @@ void processNormalKeys(unsigned char key, int x, int y) {
 //========================================
 
 int parseCommandLine(int argc, char *argv[], vector<Light*> & lights, Sphere * sphere) {
-	
+
 	bool malformedArg;
 	bool printUsage = false;
 	float r=0, g=0, b=0, x=0, y=0, z=0, v=0;
 	int i;
 	for (i = 1; i < argc; i++) {
-		
+
 			malformedArg = false;
-		
+
 			if (strcmp(argv[i],"-d") == 0) {
-				
+
 				DEBUG = 1;
-				
+
 			} else if (!strcmp(argv[i], "-ka")) {
-				
+
 				if (isThereMore(i, argc, 3)) {
 					r = atof(argv[++i]);
 					g = atof(argv[++i]);
@@ -256,10 +253,10 @@ int parseCommandLine(int argc, char *argv[], vector<Light*> & lights, Sphere * s
 				} else {
 					malformedArg = true;
 				}
-				
+
 			} else if (!strcmp(argv[i], "-kd")) {
-				
-				
+
+
 				if (isThereMore(i, argc, 3)) {
 					r = atof(argv[++i]);
 					g = atof(argv[++i]);
@@ -268,10 +265,10 @@ int parseCommandLine(int argc, char *argv[], vector<Light*> & lights, Sphere * s
 				} else {
 					malformedArg = true;
 				}
-				
+
 			} else if (!strcmp(argv[i], "-ks")) {
-				
-				
+
+
 				if (isThereMore(i, argc, 3)) {
 					r = atof(argv[++i]);
 					g = atof(argv[++i]);
@@ -280,19 +277,19 @@ int parseCommandLine(int argc, char *argv[], vector<Light*> & lights, Sphere * s
 				} else {
 					malformedArg = true;
 				}
-				
+
 			} else if (!strcmp(argv[i], "-sp")) {
-				
-				
+
+
 				if (isThereMore(i, argc, 1)) {
 					v = atof(argv[++i]);
 					sphere->setSpecularCoeff(v);
 				} else {
 					malformedArg = true;
 				}
-				
+
 			} else if (!strcmp(argv[i], "-pl")) {
-				
+
 				if (isThereMore(i, argc, 6)) {
 					x = atof(argv[++i]);
 					y = atof(argv[++i]);
@@ -304,9 +301,9 @@ int parseCommandLine(int argc, char *argv[], vector<Light*> & lights, Sphere * s
 				} else {
 					malformedArg = true;
 				}
-				
+
 			} else if (!strcmp(argv[i], "-dl")) {
-			
+
 				if (isThereMore(i, argc, 6)) {
 					x = atof(argv[++i]);
 					y = atof(argv[++i]);
@@ -318,9 +315,9 @@ int parseCommandLine(int argc, char *argv[], vector<Light*> & lights, Sphere * s
 				} else {
 					malformedArg = true;
 				}
-				
+
 			} else if (!strcmp(argv[i], "-bg")) {
-				
+
 				if (isThereMore(i, argc, 3)) {
 					r = atof(argv[++i]);
 					g = atof(argv[++i]);
@@ -329,23 +326,23 @@ int parseCommandLine(int argc, char *argv[], vector<Light*> & lights, Sphere * s
 				} else {
 					malformedArg = true;
 				}
-				
+
 			} else if (!strcmp(argv[i], "-ani")) {
-				
+
 				ANI = 1;
-				
+
 			} else if (!strcmp(argv[i], "-bumpmap")) {
-				
+
 				if (isThereMore(i, argc, 1)) {
 					BM = atoi(argv[++i]);
 				} else {
 					malformedArg = true;
 				}
-				
+
 			} else {
 				malformedArg = true;
 			}
-			
+
 			if (malformedArg) {
 				printDebug("Malformed input arg in parsing command \"" << argv[i] << "\"");
 				printUsage = true;
@@ -354,7 +351,7 @@ int parseCommandLine(int argc, char *argv[], vector<Light*> & lights, Sphere * s
 	if (printUsage)
 		return 1;
 	return 0;
-	
+
 }
 
 void printUsage() {
@@ -377,14 +374,12 @@ void move() {
 }
 
 int main(int argc, char *argv[]) {
-	
+
 	if (parseCommandLine(argc, argv, lights, &sphr)) {
 		printUsage();
 		return 1;
 	}
-	
-	bmap.loadTextFile();
-	
+
 	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
 	glutInitWindowSize(800,600);
@@ -398,9 +393,9 @@ int main(int argc, char *argv[]) {
 	glutKeyboardFunc(processNormalKeys);
 	glutIdleFunc(move);
 	glutMainLoop();							// infinite loop that will keep drawing and resizing and whatever else
-	
+
 	//cleanUp();
-	
+
 	return 0;
-	
+
 }
