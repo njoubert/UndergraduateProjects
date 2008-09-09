b991da6a7c7377017de629e050dd8b79bddfca95
Rohit <Rohit@997b1fe7-5dcb-cc6a-e049-b7da040a1fbe>
2008-09-09 08:07:37 +0000

    testing
    git-svn-id: svn+ssh://njoubert.com/var/svn@373 997b1fe7-5dcb-cc6a-e049-b7da040a1fbe

--- CS188_p1/trunk/src/search.py
+++ CS188_p1/trunk/src/search.py
@@ -4,19 +4,20 @@ import util
 class SearchProblem:
   """
   Abstract SearchProblem class. Your classes
-  should inherit from this class and override 
+  should inherit from this class and override
   all the methods below
   """
   def getStartState(self):
      """
-     Returns the start state for the search problem 
+     Returns the start state for the search problem
      """
+     print 'testing'
      util.raiseNotDefined()
-    
+
   def isGoalState(self, state):
      """
        state: Search state
-    
+
      Returns True if and only if the state is a valid goal state
      """
      util.raiseNotDefined()
@@ -24,11 +25,11 @@ class SearchProblem:
   def getSuccessors(self, state):
      """
        state: Search state
-     
-     For a given state, this should return a list of triples, 
-     (successor, action, stepCost), where 'successor' is a 
+
+     For a given state, this should return a list of triples,
+     (successor, action, stepCost), where 'successor' is a
      successor to the current state, 'action' is the action
-     required to get there, and 'stepCost' is the incremental 
+     required to get there, and 'stepCost' is the incremental
      cost of expanding to that successor
      """
      util.raiseNotDefined()
@@ -36,12 +37,12 @@ class SearchProblem:
   def getCostOfActions(self, actions):
      """
       actions: A list of actions to take
- 
+
      This method returns the total cost of a particular sequence of actions.  The sequence must
      be composed of legal moves
      """
      util.raiseNotDefined()
-           
+
 
 def tinyMazeSearch(problem):
   """Returns a sequence of moves that solves tinyMaze.  For any other
@@ -54,12 +55,12 @@ def tinyMazeSearch(problem):
 def depthFirstSearch(problem):
   """
   Search the deepest nodes in the search tree first. [p 74].
-  
+
   Your search algorithm needs to return a 3-item tuple (path, actions, totalcost)
     path: a list of states in the search problem from start to goal
     actions: the sequence of actions that reaches the goal
     totalcost: the total cost of the path
-    
+
   Of course, the actions are the most important by far; we will use these
   to determine the correctness of your algorithm.
   """
@@ -70,7 +71,7 @@ def breadthFirstSearch(problem):
   "Search the shallowest nodes in the search tree first. [p 74]"
   "*** YOUR CODE HERE ***"
   util.raiseNotDefined()
-      
+
 def uniformCostSearch(problem):
   "Search the node of least total cost first. "
   "*** YOUR CODE HERE ***"
@@ -87,7 +88,7 @@ def aStarSearch(problem, heuristic=nullHeuristic):
   "Search the node that has the lowest combined cost and heuristic first."
   "*** YOUR CODE HERE ***"
   util.raiseNotDefined()
-    
+
 def greedySearch(problem, heuristic=nullHeuristic):
   "Search the node that has the lowest heuristic first."
   "*** YOUR CODE HERE ***"
