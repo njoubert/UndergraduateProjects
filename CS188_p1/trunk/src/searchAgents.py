ecfdb5a94f28d0feb3a71a8a8a925171518555c1
Rohit <Rohit@997b1fe7-5dcb-cc6a-e049-b7da040a1fbe>
2008-09-11 05:46:02 +0000

    finished 5a Looks fine.
    git-svn-id: svn+ssh://njoubert.com/var/svn@385 997b1fe7-5dcb-cc6a-e049-b7da040a1fbe

--- CS188_p1/trunk/src/searchAgents.py
+++ CS188_p1/trunk/src/searchAgents.py
@@ -1,5 +1,5 @@
 """
-This file contains all of the agents that can be selected to 
+This file contains all of the agents that can be selected to
 control Pacman.  To select an agent, simple use the '-p' option
 when running pacman.py.  That is, to load your DepthFirstSearchAgent,
 just run the following command from the command line:
@@ -36,7 +36,7 @@ class GoWestAgent(Agent):
       return Directions.WEST
     else:
       return Directions.STOP
-    
+
 #######################################################
 # This portion is written for you, but will only work #
 #       after you fill in parts of search.py and      #
@@ -46,18 +46,18 @@ class GoWestAgent(Agent):
 class PositionSearchProblem(search.SearchProblem):
   """
   A search problem defines the state space, start state, goal test,
-  successor function and cost function.  This search problem can be 
+  successor function and cost function.  This search problem can be
   used to find paths to a particular point on the pacman board.
-  
+
   The state space consists of (x,y) positions in a pacman game.
-  
+
   This search problem is fully specified and should not require change.
   """
-  
+
   def __init__(self, gameState, costFn = lambda x: 1, goal=(1,1)):
     """
-    Stores the start and goal.  
-    
+    Stores the start and goal.
+
     gameState: A GameState object (pacman.py)
     costFn: A function from a search state (tuple) to a non-negative number
     goal: A position in the gameState
@@ -76,8 +76,8 @@ class PositionSearchProblem(search.SearchProblem):
     return self.startState
 
   def isGoalState(self, state):
-     isGoal = state == self.goal 
-     
+     isGoal = state == self.goal
+
      # For display purposes only
      if isGoal:
        print 'Goal found after expanding %d nodes.' % self._expanded
@@ -86,21 +86,21 @@ class PositionSearchProblem(search.SearchProblem):
        if '_display' in dir(__main__):
          if 'drawExpandedCells' in dir(__main__._display): #@UndefinedVariable
            __main__._display.drawExpandedCells(self._visitedlist) #@UndefinedVariable
-       
-     return isGoal   
-   
+
+     return isGoal
+
   def getSuccessors(self, state):
     """
     Returns successor states, the actions they require, and a cost of 1.
-    
+
      As noted in search.py:
-         For a given state, this should return a list of triples, 
-     (successor, action, stepCost), where 'successor' is a 
+         For a given state, this should return a list of triples,
+     (successor, action, stepCost), where 'successor' is a
      successor to the current state, 'action' is the action
-     required to get there, and 'stepCost' is the incremental 
+     required to get there, and 'stepCost' is the incremental
      cost of expanding to that successor
     """
-    
+
     successors = []
     for action in [Directions.NORTH, Directions.SOUTH, Directions.EAST, Directions.WEST]:
       x,y = state
@@ -110,13 +110,13 @@ class PositionSearchProblem(search.SearchProblem):
         nextState = (nextx, nexty)
         cost = self.costFn(nextState)
         successors.append( ( nextState, action, cost) )
-        
+
     # Bookkeeping for display purposes
-    self._expanded += 1 
+    self._expanded += 1
     if state not in self._visited:
       self._visited[state] = True
       self._visitedlist.append(state)
-      
+
     return successors
 
   def getCostOfActions(self, actions):
@@ -134,26 +134,26 @@ class PositionSearchProblem(search.SearchProblem):
         return 999999
       cost += self.costFn((x,y))
     return cost
-    
-  
+
+
 class SearchAgent(Agent):
   """
   This very general search agent finds a path using a supplied search algorithm for a
   supplied search problem, then returns actions to follow that path.
-  
+
   As a default, this agent runs DFS on a PositionSearchProblem to find location (1,1)
   """
-    
+
   def __init__(self, searchFunction=None, searchType=PositionSearchProblem):
     self.searchFunction = searchFunction
     self.searchType = searchType
-    
+
   def registerInitialState(self, state):
     """
     This is the first time that the agent sees the layout of the game board. Here, we
     choose a path to the goal.  In this phase, the agent should compute the path to the
     goal and store it in a local variable.
-    
+
     state: a GameState object (pacman.py)
     """
     if self.searchFunction == None:
@@ -170,25 +170,25 @@ class SearchAgent(Agent):
         print 'No solution found??!??!??!?! WTF!!! HELP!'
         sys.exit(1)
     print 'Path found with total cost of %d in %.1f seconds' % (problem.getCostOfActions(self.actions), time.time() - starttime)
-    
+
   def getAction(self, state):
     """
     Returns the next action in the path chosen earlier (in registerInitialState).  Return
     Directions.STOP if there is no further action to take.
-    
+
     state: a GameState object (pacman.py)
     """
     if (len(self.actions) == 0):
         return Directions.STOP
     return self.actions.pop(0)
-      
+
 class TinyMazeSearchAgent(SearchAgent):
   """
   An agent which uses tinyClassSearch to find a path (which only returns a
   correct path for tinyMaze) and follows that path.
   """
   def __init__(self):
-    SearchAgent.__init__(self, search.tinyMazeSearch)  
+    SearchAgent.__init__(self, search.tinyMazeSearch)
 
 
 class DepthFirstSearchAgent(SearchAgent):
@@ -197,7 +197,7 @@ class DepthFirstSearchAgent(SearchAgent):
   follows that path.
   """
   def __init__(self):
-    SearchAgent.__init__(self, search.depthFirstSearch)  
+    SearchAgent.__init__(self, search.depthFirstSearch)
 
 class BreadthFirstSearchAgent(SearchAgent):
   """
@@ -205,14 +205,14 @@ class BreadthFirstSearchAgent(SearchAgent):
   follows that path.
   """
   def __init__(self):
-    SearchAgent.__init__(self, search.breadthFirstSearch)  
-    
+    SearchAgent.__init__(self, search.breadthFirstSearch)
+
 class UniformCostSearchAgent(SearchAgent):
   """
   An agent that computes a path to the goal position using UCS.
   """
   def __init__(self):
-    SearchAgent.__init__(self, search.uniformCostSearch)  
+    SearchAgent.__init__(self, search.uniformCostSearch)
 
 class StayEastSearchAgent(SearchAgent):
   """
@@ -221,8 +221,8 @@ class StayEastSearchAgent(SearchAgent):
   """
   def __init__(self):
     problem = lambda x: PositionSearchProblem(x, stayEastCost)
-    SearchAgent.__init__(self, search.uniformCostSearch, problem)  
-    
+    SearchAgent.__init__(self, search.uniformCostSearch, problem)
+
 class StayWestSearchAgent(SearchAgent):
   """
   An agent that computes a path to eat all the dots using UCS, but
@@ -231,37 +231,37 @@ class StayWestSearchAgent(SearchAgent):
   """
   def __init__(self):
     problem = lambda x: PositionSearchProblem(x, stayWestCost)
-    SearchAgent.__init__(self, search.uniformCostSearch, problem)  
+    SearchAgent.__init__(self, search.uniformCostSearch, problem)
 
 def stayEastCost(position):
   """
   Gives a cost for each (x,y) position that guides a search agent eastward
   """
-  return .5 ** position[0]  
-  
+  return .5 ** position[0]
+
 def stayWestCost(position):
   """
   Gives a cost for each (x,y) position that guides a search agent westward
   """
-  return 2 ** position[0]  
+  return 2 ** position[0]
 
 class FoodSearchProblem:
   """
-  A search problem associated with finding the a path that collects all of the 
+  A search problem associated with finding the a path that collects all of the
   food (dots) in a Pacman game.
-  
+
   A search state in this problem is a tuple ( pacmanPosition, foodGrid ) where
     pacmanPosition: a tuple (x,y) of integers specifying Pacman's position
-    foodGrid:       a Grid (see game.py) of either True or False, specifying remaining food 
+    foodGrid:       a Grid (see game.py) of either True or False, specifying remaining food
   """
   def __init__(self, state):
     self.start = (state.getPacmanPosition(), state.getFood())
     self.walls = state.getWalls()
     self._expanded = 0
-      
+
   def getStartState(self):
     return self.start
-  
+
   def isGoalState(self, state):
     if state[1].count() == 0:
       print 'Goal found after expanding %d nodes.' % self._expanded
@@ -269,16 +269,16 @@ class FoodSearchProblem:
     return False
 
   def getSuccessors(self, state):
-    """    
+    """
     Returns successor states, the actions they require, and a cost of 1.
-    
+
      As noted in search.py:
-         For a given state, this should return a list of triples, 
-     (successor, action, stepCost), where 'successor' is a 
+         For a given state, this should return a list of triples,
+     (successor, action, stepCost), where 'successor' is a
      successor to the current state, 'action' is the action
-     required to get there, and 'stepCost' is the incremental 
+     required to get there, and 'stepCost' is the incremental
      cost of expanding to that successor
-     
+
     """
     successors = []
     self._expanded += 1
@@ -305,13 +305,13 @@ class FoodSearchProblem:
         return 999999
       cost += 1
     return cost
-     
+
 class UniformCostFoodSearchAgent(SearchAgent):
   """
   An agent that computes a path to eat all the dots using UCS.
   """
   def __init__(self):
-    SearchAgent.__init__(self, search.uniformCostSearch, FoodSearchProblem)  
+    SearchAgent.__init__(self, search.uniformCostSearch, FoodSearchProblem)
 
 def manhattanAStar(problem):
   """
@@ -325,7 +325,7 @@ class ManhattanAStarSearchAgent(SearchAgent):
   the Manhattan distance heuristic.
   """
   def __init__(self):
-    SearchAgent.__init__(self, manhattanAStar, PositionSearchProblem)  
+    SearchAgent.__init__(self, manhattanAStar, PositionSearchProblem)
 
 def trivialFoodHeuristic(state):
   """
@@ -336,24 +336,24 @@ def trivialFoodHeuristic(state):
   else:
     return 1
 
-###########################################################    
+###########################################################
 # You have to fill in several parts of the following code #
-###########################################################    
+###########################################################
 
 def getFoodHeuristic(gameState):
   """
-  Instead of filling in the foodHeuristic function directly, you can fill in 
+  Instead of filling in the foodHeuristic function directly, you can fill in
   this function which takes a full gameState for Pacman (see pacman.py) and
   returns a heuristic function.  The heuristic function must
     - take a single parameter, a search state
     - return a non-negative number that is the value of the heuristic at that state
 
-  This function is *only* here for students who want to create more complex 
+  This function is *only* here for students who want to create more complex
   heuristics that use aspects of the gameState other than the food Grid and
   Pacman's location (such as where the walls are, etc.)
-    
-  Note: The state that will be passed to your heuristic function is a tuple 
-  ( pacmanPosition, foodGrid ) where foodGrid is a Grid (see game.py) of either 
+
+  Note: The state that will be passed to your heuristic function is a tuple
+  ( pacmanPosition, foodGrid ) where foodGrid is a Grid (see game.py) of either
   True or False.
   """
   # If you don't want to implement this method, you can leave this default implementation
@@ -362,40 +362,48 @@ def getFoodHeuristic(gameState):
 def foodHeuristic(state):
   """
   Here, you can write your food heuristic function instead of using getFoodHeuristic.
-  This heuristic must be admissible (if your AStarFoodSearchAgent and your 
-  UniformCostSearchAgent *ever* find solutions of different length, your heuristic 
-  is *not* admissible).  
-  
-  The state is a tuple ( pacmanPosition, foodGrid ) where foodGrid is a 
+  This heuristic must be admissible (if your AStarFoodSearchAgent and your
+  UniformCostSearchAgent *ever* find solutions of different length, your heuristic
+  is *not* admissible).
+
+  The state is a tuple ( pacmanPosition, foodGrid ) where foodGrid is a
   Grid (see game.py) of either True or False.
-  
+
   Note that this function *does not* have access to the location of walls, capsules,
   ghosts, etc.  If you want to work with this information, you should implement
   getFoodHeuristic instead of this function.
-  
-  Hint: getFoodHeuristic can return a heuristic that encapsulates data through a 
-  function closure (like the manhattanAStar function above).  If you don't know how 
+
+  Hint: getFoodHeuristic can return a heuristic that encapsulates data through a
+  function closure (like the manhattanAStar function above).  If you don't know how
   this works, come to office hours.
   """
   "*** YOUR CODE HERE ***"
   util.raiseNotDefined()
 
+def foodAStar(problem):
+  """
+  A wrapper for A* that uses the Manhattan distance heuristic.
+  """
+  return search.aStarSearch(problem, getFoodHeuristic(problem.getStartState()))
+
 class AStarFoodSearchAgent(SearchAgent):
   """
   An agent that computes a path to eat all the dots using AStar.
-  
+
   You should use either foodHeuristic or getFoodHeuristic in your code here.
   """
 
   "*** YOUR CODE HERE ***"
+  def __init__(self, searchFunction, searchType):
+    SearchAgent.__init__(self, foodAStar, searchType)
 
 class GreedyFoodSearchAgent(SearchAgent):
   """
   An agent that computes a path to eat all the dots using greedy search.
   """
   "*** YOUR CODE HERE ***"
-  
-  
+
+
 
 
 class TrivialAStarFoodSearchAgent(AStarFoodSearchAgent):
