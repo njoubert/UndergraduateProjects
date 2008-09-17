26f9a6310e9740354f8e661e504db5c72b08ab8b
njoubert <njoubert@997b1fe7-5dcb-cc6a-e049-b7da040a1fbe>
2008-09-17 05:08:52 +0000

    Organzation files added.
    git-svn-id: svn+ssh://njoubert.com/var/svn@413 997b1fe7-5dcb-cc6a-e049-b7da040a1fbe

---
+++ RSCH_ClothSym/trunk/Debug/src/System.d
@@ -0,0 +1,6 @@
+src/System.d src/System.o: ../src/System.cpp ../src/System.h \
+  ../src/global.h
+
+../src/System.h:
+
+../src/global.h:
---
+++ RSCH_ClothSym/trunk/Debug/src/main.d
@@ -0,0 +1,10 @@
+src/main.d src/main.o: ../src/main.cpp ../src/main.h ../src/System.h \
+  ../src/global.h ../src/Solver.h
+
+../src/main.h:
+
+../src/System.h:
+
+../src/global.h:
+
+../src/Solver.h:
