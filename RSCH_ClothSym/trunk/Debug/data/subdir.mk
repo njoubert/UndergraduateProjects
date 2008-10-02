71ca6e9d591495deccb246f203952df055cebd63
njoubert <njoubert@997b1fe7-5dcb-cc6a-e049-b7da040a1fbe>
2008-10-02 00:17:32 +0000

    git-svn-id: svn+ssh://njoubert.com/var/svn@478 997b1fe7-5dcb-cc6a-e049-b7da040a1fbe

--- RSCH_ClothSym/trunk/Debug/data/subdir.mk
+++ RSCH_ClothSym/trunk/Debug/data/subdir.mk
@@ -3,13 +3,7 @@
 ################################################################################
 
 # Add inputs and outputs from these tool invocations to the build variables 
-OBJ_SRCS += \
-../data/chameleon1.obj \
-../data/face1.obj \
-../data/flatGridTest.obj \
-../data/grid1.obj \
-../data/gridTest.obj \
-../data/isosahedron.obj 
+OBJ_SRCS += 
 
 
 # Each subdirectory must supply rules for building sources it contributes
