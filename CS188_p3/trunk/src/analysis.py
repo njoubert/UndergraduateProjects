aa8254f655ff531bcfdc8d92643e42575571fe8e
njoubert <njoubert@997b1fe7-5dcb-cc6a-e049-b7da040a1fbe>
2008-10-13 07:41:51 +0000

    git-svn-id: svn+ssh://njoubert.com/var/svn@507 997b1fe7-5dcb-cc6a-e049-b7da040a1fbe

--- CS188_p3/trunk/src/analysis.py
+++ CS188_p3/trunk/src/analysis.py
@@ -7,27 +7,28 @@
 ########################
 
 def question2():
-  YOUR_ANSWER = None
+  YOUR_ANSWER = 7
   return YOUR_ANSWER
 
 def question3():
-  YOUR_ANSWER = None
+  YOUR_ANSWER = (0.9, 0)
   return YOUR_ANSWER
   
+#(discount, noise, living reward)  
 def question4a():
-  YOUR_ANSWER = None
+  YOUR_ANSWER = (0.9, 0.0, -5.0)
   return YOUR_ANSWER
 
 def question4b():
-  YOUR_ANSWER = None
+  YOUR_ANSWER = 'NOT POSSIBLE'
   return YOUR_ANSWER
 
 def question4c():
-  YOUR_ANSWER = None
+  YOUR_ANSWER = (0.5, 0.0, 0.0)
   return YOUR_ANSWER
 
 def question4d():
-  YOUR_ANSWER = None
+  YOUR_ANSWER = (0.9,0.1,0.6)
   return YOUR_ANSWER
 
 def question4e():
