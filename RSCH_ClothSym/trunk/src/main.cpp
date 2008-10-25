71afcc1fff16dd792f503f94420fbd85d3e09260
njoubert <njoubert@997b1fe7-5dcb-cc6a-e049-b7da040a1fbe>
2008-10-25 07:46:57 +0000

    Nice camera movement implemented! We still need to auto-size according to incoming objects.
    git-svn-id: svn+ssh://njoubert.com/var/svn@581 997b1fe7-5dcb-cc6a-e049-b7da040a1fbe

--- RSCH_ClothSym/trunk/src/main.cpp
+++ RSCH_ClothSym/trunk/src/main.cpp
@@ -5,36 +5,155 @@ using namespace std;
 World world;
 ImageSaver imagesaver;
 
-class Viewport {
+//-------------------------------------------------------------------------------
+//
+class Camera {
 public:
-    Viewport() {
-        rotateX = rotateY = rotateZ = translateX = translateY = translateZ = 0.0f;
-        rotateX = 0;
-        rotateY = 0;
-        rotateZ = 0;
-        translateX = 0;
-        translateY = 0;
-        translateZ = 0;
-        wireFrame = false;
+	Camera() {
+		_fovy = 30;
+		_zNear = 1;
+		_zFar = 100;
+
+		_left = -10;
+		_right = 10;
+		_top = -10;
+		_bottom = 10;
+		_nearVal = 1;
+		_farVal = 1000;
+
+		_zoom = 40.0f;
+		_rotx = 10.0f;
+		_roty = 0.0f;
+		_tx = 0.0f;
+		_ty = 0.0f;
+
+		_lastx = _lasty = 0;
+
+		_Buttons[0] = _Buttons[1] = _Buttons[2] = 0;
+
+		wireFrame = false;
         paused = true;
         inverseFPS = 1.0 / 25.0;
         lastTime = 0;
-    }
-	int w, h; // width and height
-	float rotateX;
-	float rotateY;
-	float rotateZ;
-	float translateX;
-	float translateY;
-	float translateZ;
+	}
+
+	void setPerspective(int w, int h) {
+		_w = w;
+		_h = h;
+		glMatrixMode(GL_PROJECTION);
+		glLoadIdentity();
+		//glOrtho(_left, _right, _bottom, _top, _nearVal, _farVal);
+		gluPerspective(_fovy,(float)w/h, _zNear, _zFar);
+	}
+
+	void setView() {
+		glMatrixMode(GL_MODELVIEW);
+		glLoadIdentity();
+		/*
+		 //You can set the position of the camera qith gluLookAt
+		 gluLookAt(0.0,0,0,1.0,
+		 0.0,0.0,0.0,
+		 0.0,1.0,0.0);
+		 */
+		//This order gives you Maya-like
+		//mouse control of your camera
+		glTranslatef(0,0,-_zoom);
+		glTranslatef(_tx,_ty,0);
+		glRotatef(_rotx,1,0,0);
+		glRotatef(_roty,0,1,0);
+
+	}
+
+	void mousemotion(int x,int y) {
+		int diffx=x-_lastx;
+		int diffy=y-_lasty;
+		_lastx=x;
+		_lasty=y;
+
+		if( _Buttons[2] ) {
+			_zoom -= (float) 0.05f * diffx;
+		} else
+			if( _Buttons[0] )
+			{
+				_rotx += (float) 0.5f * diffy;
+				_roty += (float) 0.5f * diffx;
+			}
+			else
+				if( _Buttons[2] )
+				{
+					_tx += (float) 0.05f * diffx;
+					_ty -= (float) 0.05f * diffy;
+				}
+		glutPostRedisplay();
+	}
+
+	void mousepress(int b,int s,int x,int y){
+		_lastx=x;
+		_lasty=y;
+		switch(b)
+		{
+			case GLUT_LEFT_BUTTON:
+				_Buttons[0] = ((GLUT_DOWN==s)?1:0);
+				break;
+			case GLUT_MIDDLE_BUTTON:
+				_Buttons[1] = ((GLUT_DOWN==s)?1:0);
+				break;
+			case GLUT_RIGHT_BUTTON:
+				_Buttons[2] = ((GLUT_DOWN==s)?1:0);
+				break;
+			default:
+				break;
+		}
+		glutPostRedisplay();
+	}
+
+	void keypress(int key, int x, int y) {
+		switch (key) {
+			case '=':
+				_zoom -= 1.0;
+				break;
+			case '-':
+				_zoom += 1.0;
+				break;
+		}
+	}
+
+	void specialkeypress(int key, int x, int y) {
+		switch(key) {
+			case GLUT_KEY_UP:
+				_ty -= 1.0;
+				break;
+			case GLUT_KEY_DOWN:
+				_ty += 1.0;
+				break;
+			case GLUT_KEY_RIGHT:
+				_tx -= 1.0;
+				break;
+			case GLUT_KEY_LEFT:
+				_tx += 1.0;
+				break;
+		}
+	}
+
+
+private:
+	float _fovy, _zNear, _zFar;
+	float _left, _right, _bottom, _top, _nearVal, _farVal;
+	float _zoom, _rotx, _roty, _tx, _ty;
+	int _lastx, _lasty;
+	unsigned char _Buttons[3];
+public:
+	int _w, _h;
 	bool wireFrame;
 	double lastTime;
     double inverseFPS;
     bool paused;
 };
 
-Viewport    viewport;
+Camera cam;
 
+//-------------------------------------------------------------------------------
+//
 void printUsage() {
     cout << "Usage: ";
     cout << " ClothSym " << endl;
@@ -43,8 +162,9 @@ void printUsage() {
     cout << "      [-img directory] " << endl;
 }
 
+//-------------------------------------------------------------------------------
+//
 int parseCommandLine(int argc, char *argv[]) {
-
     bool malformedArg;
     bool printUsage = false;
     bool hasOBJ = false;
@@ -95,7 +215,7 @@ int parseCommandLine(int argc, char *argv[]) {
 
             if (isThereMore(i, argc, 1)) {
                 std::string dirname = std::string(argv[++i]);
-                imagesaver.initialize(dirname, viewport.inverseFPS);
+                imagesaver.initialize(dirname, cam.inverseFPS);
             } else {
                 malformedArg = true;
             }
@@ -115,54 +235,22 @@ int parseCommandLine(int argc, char *argv[]) {
 
 }
 
+//-------------------------------------------------------------------------------
+//
 void processSpecialKeys(int key, int x, int y) {
-    switch(key) {
-    case GLUT_KEY_UP:
-        viewport.translateY -= .1f;
-        break;
-    case GLUT_KEY_DOWN:
-        viewport.translateY += .1f;
-        break;
-    case GLUT_KEY_RIGHT:
-        viewport.translateX -= .1f;
-        break;
-    case GLUT_KEY_LEFT:
-        viewport.translateX += .1f;
-        break;
-    }
+	cam.specialkeypress(key,x,y);
 }
 
+//-------------------------------------------------------------------------------
+//
 void processKeys(unsigned char key, int x, int y) {
+	cam.keypress(key,x,y);
     switch (key) {
-    case 'w':
-        viewport.rotateY -= 1.5f;
-        break;
-    case 's':
-        viewport.rotateY += 1.5f;
-        break;
-    case 'a':
-        viewport.rotateZ -= 1.5f;
-        break;
-    case 'd':
-        viewport.rotateZ += 1.5f;
-        break;
-    case 'q':
-        viewport.rotateX -= 1.5f;
-        break;
-    case 'e':
-        viewport.rotateX += 1.5f;
-        break;
-    case '-':
-        viewport.translateZ -= 2.5f;
-        break;
-    case '=':
-        viewport.translateZ += 2.5f;
-        break;
     case 'v':
-        viewport.wireFrame = !viewport.wireFrame;
+        cam.wireFrame = !cam.wireFrame;
         break;
     case 'p':
-        viewport.paused = !viewport.paused;
+        cam.paused = !cam.paused;
         break;
     case 27:
         exit(0);
@@ -170,10 +258,12 @@ void processKeys(unsigned char key, int x, int y) {
     }
 }
 
+//-------------------------------------------------------------------------------
+//
 void init(void)
 {
-   GLfloat mat_diffuse[] = { 0.8, 0.0, 0.8, 1.0 };
-   GLfloat mat_ambient[] = { 0.6, 0.0, 0.6, 1.0 };
+   GLfloat mat_diffuse[] = { 0.5, 0.5, 0.5, 1.0 };
+   GLfloat mat_ambient[] = { 0.5, 0.5, 0.5, 1.0 };
    GLfloat mat_specular[] = { 0.1, 0.1, 0.1, 1.0 };
    GLfloat mat_shininess[] = { 50.0 };
    GLfloat light_position[] = { 0.0, 0.0, 10.0, 0.0 };
@@ -197,93 +287,92 @@ void init(void)
    glEnable(GL_DEPTH_TEST);
 }
 
-void Blue () {
-	GLfloat mat_diffuse[] = { 0.0, 1.0, 0.0, 1.0 };
-	GLfloat mat_ambient[] = { 0.0, 0.1, 0.0, 1.0 };
+//-------------------------------------------------------------------------------
+//All this shit is very much illegal in terms of memory usage! those are POINTERS...
+void Green () {
+	GLfloat mat_diffuse[] = {0.0, 1.0, 0.0, 1.0};
+	GLfloat mat_ambient[] = {0.0, 0.1, 0.0, 1.0};
 	glMaterialfv(GL_FRONT_AND_BACK, GL_DIFFUSE, mat_diffuse);
 	glMaterialfv(GL_FRONT_AND_BACK, GL_AMBIENT, mat_ambient);
 }
-
 void Pink () {
-	GLfloat mat_diffuse[] = { 0.8, 0.0, 0.8, 1.0 };
-	GLfloat mat_ambient[] = { 0.6, 0.0, 0.6, 1.0 };
+	GLfloat mat_diffuse[] = {0.8, 0.0, 0.4, 1.0};
+	GLfloat mat_ambient[] = {0.8, 0.0, 0.4, 1.0};
 	glMaterialfv(GL_FRONT_AND_BACK, GL_DIFFUSE, mat_diffuse);
-	glMaterialfv(GL_FRONT_AND_BACK, GL_DIFFUSE, mat_ambient);
+	glMaterialfv(GL_FRONT_AND_BACK, GL_AMBIENT, mat_ambient);
 }
-
-/**
- * This function draws the actual world using OpenGL.
- */
+void White () {
+	GLfloat mat_diffuse[] = {1.0, 1.0, 1.0, 1.0};
+	GLfloat mat_ambient[] = {1.0, 1.0, 1.0, 1.0};
+	glMaterialfv(GL_FRONT_AND_BACK, GL_DIFFUSE, mat_diffuse);
+	glMaterialfv(GL_FRONT_AND_BACK, GL_AMBIENT, mat_ambient);
+}
+void Grey () {
+	GLfloat mat_diffuse[] = {0.6, 0.0, 0.6, 1.0};
+	GLfloat mat_ambient[] = {0.06, 0.06, 0.6, 1.0};
+	glMaterialfv(GL_FRONT_AND_BACK, GL_DIFFUSE, mat_diffuse);
+	glMaterialfv(GL_FRONT_AND_BACK, GL_AMBIENT, mat_ambient);
+}
+//-------------------------------------------------------------------------------
+// This function draws the actual world using OpenGL.
+//
 void display(void)
 {
-   glClear (GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-   glLoadIdentity();
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	cam.setView();
 
-   //Apply the camera transformations
-   glTranslatef(viewport.translateX,
-           viewport.translateY,
-           viewport.translateZ);
+	glPushMatrix();
 
-   glRotatef(viewport.rotateX, 1.0f, 0.0f, 0.0f);
-   glRotatef(viewport.rotateY, 0.0f, 1.0f, 0.0f);
-   glRotatef(viewport.rotateZ, 0.0f, 0.0f, 1.0f);
-
-   if (viewport.wireFrame) {
+   if (cam.wireFrame) {
        glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
    } else {
        glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
    }
 
-   //Draw the world!
-   world.draw();
+	White();
+	// draw grid
+	glBegin(GL_LINES);
+	for(int i=-10;i<=10;++i) {
+		glVertex3f(i,0,-10);
+		glVertex3f(i,0,10);
 
-   glFlush ();
-   glutSwapBuffers();
+		glVertex3f(10,0,i);
+		glVertex3f(-10,0,i);
+	}
+	glEnd();
+	Pink();
+
+	//Draw the world!
+	world.draw();
+
+	glPopMatrix();
+
+	glFlush ();
+	glutSwapBuffers();
 }
 
-void reshape (int w, int h)
-{
-   glViewport (0, 0, (GLsizei) w, (GLsizei) h);
-   glMatrixMode (GL_PROJECTION);
-   glLoadIdentity();
-   viewport.w = w;
-   viewport.h = h;
-   /*
-   if (w <= h)
-      glOrtho (-1*x, x, -1*x*(GLfloat)h/(GLfloat)w,
-         x*(GLfloat)h/(GLfloat)w, -400.0, 400.0);
-   else
-      glOrtho (-1*x*(GLfloat)w/(GLfloat)h,
-         x*(GLfloat)w/(GLfloat)h, -1*x, x, -400.0, 400.0);
-   */
-   glOrtho (-200, 200, -200, 200, -400.0, 400.0);
-   glMatrixMode(GL_MODELVIEW);
-   glLoadIdentity();
+//-------------------------------------------------------------------------------
+//
+void reshape (int w, int h) {
+	glViewport (0, 0, (GLsizei) w, (GLsizei) h);
+	cam.setPerspective(w,h);
+	cam.setView();
 }
 
-
-/**
- * Calculates the next frame of the world.
- */
+//-------------------------------------------------------------------------------
+// Calculates the next frame of the world.
+//
 void myframemove() {
-    imagesaver.saveFrame(world.getTime(), false, viewport.inverseFPS, viewport.w, viewport.h);
-    world.advance(viewport.inverseFPS);
-
-    //REMOVE THIS:
-    Blue();
-    glBegin(GL_QUADS);
-        glNormal3f(0, 0, 1);
-        glVertex3f(-10, 10, -3);
-        glVertex3f(-10, -10, -3);
-        glVertex3f(10, -10, -3);
-        glVertex3f(10, 10, -3);
-    glEnd();
-    Pink();
-
+    imagesaver.saveFrame(world.getTime(), false, cam.inverseFPS, cam._w, cam._h);
+    world.advance(cam.inverseFPS);
     glutPostRedisplay();
 }
 
+//-------------------------------------------------------------------------------
+//
 void myMousePress(int button, int state, int x, int y) {
+	cam.mousepress(button,state,x,y);
+
     if (button == GLUT_LEFT_BUTTON) {
         if (state == GLUT_DOWN) {
             //Find the point
@@ -308,7 +397,10 @@ void myMousePress(int button, int state, int x, int y) {
     }
 }
 
+//-------------------------------------------------------------------------------
+//
 void myMouseMove(int x, int y) {
+	cam.mousemotion(x,y);
 	/*
     if (sys->isMouseEnabled()) {
         float z;
@@ -327,6 +419,8 @@ void myMouseMove(int x, int y) {
 	*/
 }
 
+//-------------------------------------------------------------------------------
+//
 int main(int argc, char *argv[]) {
 
     if (parseCommandLine(argc, argv)) {
@@ -334,11 +428,11 @@ int main(int argc, char *argv[]) {
         exit(1);
     }
 
-    viewport.w = 500;
-    viewport.h = 500;
+    cam._w = 500;
+    cam._h = 500;
     glutInit(&argc, argv);
     glutInitDisplayMode (GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
-    glutInitWindowSize (viewport.w, viewport.h);
+    glutInitWindowSize (cam._w, cam._h);
     glutInitWindowPosition (100, 100);
     glutCreateWindow (argv[0]);
     init ();
