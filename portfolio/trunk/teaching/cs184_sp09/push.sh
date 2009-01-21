9dbd086efa95be516c096ac13a62076f4549cad4
njoubert <njoubert@997b1fe7-5dcb-cc6a-e049-b7da040a1fbe>
2009-01-21 08:38:02 +0000

    Additions to CS184 TA SP09
    git-svn-id: svn+ssh://njoubert.com/var/svn@811 997b1fe7-5dcb-cc6a-e049-b7da040a1fbe

--- portfolio/trunk/teaching/cs184_sp09/push.sh
+++ portfolio/trunk/teaching/cs184_sp09/push.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
+echo '\n\nCommitting...\n\n'
 svn commit -m "Additions to CS184 TA SP09"
-ssh njoubert.com "cd /var/www/teaching/cs184_sp09; ls -la; echo 'Updating...' ; svn update"
+ssh njoubert.com "cd /var/www/teaching/cs184_sp09; ls -la; echo '\n\nUpdating...\n\n' ; svn update"
