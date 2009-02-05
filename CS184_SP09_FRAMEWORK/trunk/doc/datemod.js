7993936e5610258eb8faa02b3075e47c4fc243e3
njoubert <njoubert@997b1fe7-5dcb-cc6a-e049-b7da040a1fbe>
2009-02-05 01:20:57 +0000

    git-svn-id: svn+ssh://njoubert.com/var/svn@859 997b1fe7-5dcb-cc6a-e049-b7da040a1fbe

--- CS184_SP09_FRAMEWORK/trunk/doc/datemod.js
+++
@@ -1,9 +0,0 @@
-// Formatted last-modified date
-// Jim Dettwiler  http://www.icehouse.net/jim_d/
-if (Date.parse(document.lastModified) != 0) {
-  var modiDate = new Date(document.lastModified);
-  var monthName = new Array("January", "February", "March", "April", "May", 
-   "June", "July", "August", "September", "October", "November", "December");
-  document.write("Last modified: " + monthName[modiDate.getMonth()] + " ");
-  document.write(modiDate.getDate() + ", " + modiDate.getFullYear());
-  }
--- CS184_SP09_FRAMEWORK/trunk/doc/push.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-scp * cs184@cory.cs.berkeley.edu:public_html/sp09/framework/
