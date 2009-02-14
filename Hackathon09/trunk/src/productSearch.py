2b07cab109bd0e5d8daa89445c77b874bc5e8700
hackers <hackers@997b1fe7-5dcb-cc6a-e049-b7da040a1fbe>
2009-02-14 12:20:09 +0000

    Bug fixes. Cleaned up return values.
    git-svn-id: svn+ssh://njoubert.com/var/svn@918 997b1fe7-5dcb-cc6a-e049-b7da040a1fbe

--- Hackathon09/trunk/src/productSearch.py
+++ Hackathon09/trunk/src/productSearch.py
@@ -2,25 +2,26 @@ import re
 from urllib2 import urlopen
 import gdata.base.service
 
+# returns: name of product, or None if none exist
 def getProductName(upc):
     BASE_URL = "http://www.upcdatabase.com/item/"
     REGEX_DESCRIPTION = "(<tr><td>Description</td><td></td><td>)"
     REGEX_DESCRIPTION2 = "(</td>){1}"
-    name = ""
-    
+    name = None
+        
     req = urlopen(BASE_URL+upc)
     html = req.read()
     m = re.search(REGEX_DESCRIPTION, html)
     if m != None:
         startPos = m.start() + len(REGEX_DESCRIPTION) - 2
         html = html[startPos:]
-    n = re.search(REGEX_DESCRIPTION2, html)
-    if n != None:
-        endPos = n.start()
-        name = html[:endPos]
+        n = re.search(REGEX_DESCRIPTION2, html)
+        if n != None:
+            endPos = n.start()
+            name = html[:endPos].replace('&quot;','')
     return name
 
-
+# returns: google product item, or None if none exist
 def getGoogleProduct(upc, results=30):
     gb_client = gdata.base.service.GBaseService() 
     gb_client.ClientLogin('hackathon09@gmail.com', 'hackathonyozio');
@@ -29,13 +30,16 @@ def getGoogleProduct(upc, results=30):
     q['start-index'] = '1'
     q['max-results'] = str(results)
     q.bq = str(upc)
-    feed = gb_client.QuerySnippetsFeed(q.ToUri())
+    try:
+        feed = gb_client.QuerySnippetsFeed(q.ToUri())
+    except:
+        return None
 
     itemList = []
     for entry in feed.entry:
         i = 0
         for attribute in entry.item_attributes:
-            if attribute.name == "upc":
+            if attribute.name == "upc" and attribute.text == upc:
                 i+=1
             if attribute.name == "price":
                 price = attribute.text
@@ -44,24 +48,19 @@ def getGoogleProduct(upc, results=30):
             itemList.append([entry.title.text, entry.author[0].name.text, price])
 
     # item := [product name, merchant, price]
-    minPriceItem = itemList[0]
-    for item in itemList:
-        itemPrice = item[2]
-        if itemPrice < minPriceItem[2]:
-            minPriceItem = item
-
-    return minPriceItem
+    if len(itemList) > 0:
+        minPriceItem = itemList[0]
+        for item in itemList:
+            itemPrice = item[2]
+            if itemPrice < minPriceItem[2]:
+                minPriceItem = item
+        return minPriceItem
+    return None
 
 
 # Testing
-print "-----"
-upc = '410000039144'
-name = getProductName(upc).replace('&quot;','')
-print name
-print "\ncheapest item: " + str(getGoogleProduct(upc, 30))
-
-print "----"
-upc = "013803050844"
-name = getProductName(upc).replace('&quot;','')
-print name
-print "\ncheapest item: " + str(getGoogleProduct(upc, 30))
+testCases = ["", "2", "410000039144", "013803050844"]
+for upc in testCases:
+    name = getProductName(upc)
+    print "Product name from upc: " + str(name)
+    print "Cheapest item: " + str(getGoogleProduct(upc, 30))
