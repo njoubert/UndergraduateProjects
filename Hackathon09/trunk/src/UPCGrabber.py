0d5007c7f8e1313fa4d0bf0bba9b82ec87e4ac33
hackers <hackers@997b1fe7-5dcb-cc6a-e049-b7da040a1fbe>
2009-02-14 11:46:09 +0000

    Made this into a function and organized imports
    git-svn-id: svn+ssh://njoubert.com/var/svn@912 997b1fe7-5dcb-cc6a-e049-b7da040a1fbe

--- Hackathon09/trunk/src/UPCGrabber.py
+++ Hackathon09/trunk/src/UPCGrabber.py
@@ -1,45 +1,24 @@
 import re
-import cgi
-import gdata.base.service
-import gdata.service
-try:
-    from xml.etree import ElementTree
-except ImportError:
-    from elementtree import ElementTree
-import atom
-import gdata.base
 from urllib2 import urlopen
-
-class UPCGrabber():
+            
+def getProductName(upc):
     BASE_URL = "http://www.upcdatabase.com/item/"
-    upc = ""
-    html = ""
-    description = ""
     REGEX_DESCRIPTION = "(<tr><td>Description</td><td></td><td>)"
     REGEX_DESCRIPTION2 = "(</td>){1}"
-   
-    def __init__(self):
-        print 'Parser Created'
-       
-    def setUPC(self, upc):
-        self.upc = upc
-       
-    def processUPC(self):
-        req = urlopen(self.BASE_URL+self.upc)
-        self.html = req.read()
-        m = re.search(self.REGEX_DESCRIPTION, self.html)
-        if m != None:
-            startPos = m.start() + len(self.REGEX_DESCRIPTION) - 2
-            print 'start: ' + str(startPos)
-            self.html = self.html[startPos:]
-        n = re.search(self.REGEX_DESCRIPTION2, self.html)
-        if n != None:
-            endPos = n.start()
-            print 'end: ' + str(endPos)
-            self.description = self.html[:endPos]
+    name = ""
+    
+    req = urlopen(BASE_URL+upc)
+    html = req.read()
+    m = re.search(REGEX_DESCRIPTION, html)
+    if m != None:
+        startPos = m.start() + len(REGEX_DESCRIPTION) - 2
+        html = html[startPos:]
+    n = re.search(REGEX_DESCRIPTION2, html)
+    if n != None:
+        endPos = n.start()
+        name = html[:endPos]
+    return name
 
-grabber = UPCGrabber()
-grabber.setUPC("013803050844")
-grabber.processUPC()
-base =  grabber.description.replace('&quot;','')
-print base
+
+name = getProductName("013803050844").replace('&quot;','')
+print name
