cd5a9e1b60f9166e2b7eca74bcff81876c5c2d2e
sburke <sburke@997b1fe7-5dcb-cc6a-e049-b7da040a1fbe>
2009-02-21 05:15:02 +0000

    Parallelized vector operations
    git-svn-id: svn+ssh://njoubert.com/var/svn@955 997b1fe7-5dcb-cc6a-e049-b7da040a1fbe

--- RSCH_ClothSym/trunk/src/math/LargeVector.h
+++ RSCH_ClothSym/trunk/src/math/LargeVector.h
@@ -17,30 +17,68 @@
 #include <iostream>
 #include "algebra3.h"
 
+#ifdef ENABLE_OMP
+    #include <omp.h>
+#endif
+
 class LargeVec3Vector {
 public:
     LargeVec3Vector(int n) :
         _size(n), _elements(n) {
 
+#ifdef ENABLE_OMP
+	omp_set_num_threads(ENABLE_OMP);
+#endif
+
     }
     LargeVec3Vector(int n, double v):
-        _size(n), _elements(n, vec3(v)){
+        _size(n), _elements(n, vec3(v)) {
     }
+
     LargeVec3Vector(const LargeVec3Vector & v) :
         _size(v.size()), _elements(v.size()) {
-        for (int i = 0; i < v.size(); i++) {
-            this->_elements[i] = v[i]; //Depends on the copy constructor of its contents
+#ifdef ENABLE_OMP
+	#pragma omp parallel
+	{
+        int blockSize = ceil((double) v.size() / omp_get_num_threads());
+        int id = omp_get_thread_num();
+        int st = id * blockSize;
+        int en = min(((id+1) * blockSize)-1, v.size() -1);
+        for (int i=st; i <= en; i++) {
+            _elements[i] = v[i];
         }
+	}
+#else
+	  for (int i = 0; i < v.size(); i++) {
+		  _elements[i] = v[i]; //Depends on the copy constructor of its contents
+	  }
+#endif
     }
+
     virtual ~LargeVec3Vector() {
         //Do i need something here?
     }
+
     void zeroValues() {
-        for (int i = 0; i < _size; i++)
+#ifdef ENABLE_OMP
+	#pragma omp parallel
+	{
+        int blockSize = ceil((double) _size / omp_get_num_threads());
+        int id = omp_get_thread_num();
+        int st = id * blockSize;
+        int en = min(((id+1) * blockSize)-1, _size -1);
+        for (int i=st; i <= en; i++) {
             _elements[i] = vec3(0);
+        }
+	}
+#else
+    	for (int i = 0; i < _size; i++)
+            _elements[i] = vec3(0);
+#endif
     }
+
     vec3 & operator[](int n) {
-        return _elements[n];
+    	return _elements[n];
     }
     vec3 operator[](int n) const {
         return _elements[n];
@@ -48,41 +86,122 @@ public:
     int size() const {
         return _size;
     }
+
     LargeVec3Vector & operator =(LargeVec3Vector const &v) {
-        for (int i = 0; i < _size; i++) {
+#ifdef ENABLE_OMP
+	#pragma omp parallel
+	{
+        int blockSize = ceil((double) _size / omp_get_num_threads());
+        int id = omp_get_thread_num();
+        int st = id * blockSize;
+        int en = min(((id+1) * blockSize)-1, _size -1);
+        for (int i=st; i <= en; i++) {
+            _elements[i] = v[1];
+        }
+	}
+#else
+    	for (int i = 0; i < _size; i++) {
             _elements[i] = v[i];
         }
+#endif
         return *this;
     }
     LargeVec3Vector & operator +=(LargeVec3Vector const &v) {
-        for (int i = 0; i < _size; i++) {
+#ifdef ENABLE_OMP
+	#pragma omp parallel
+	{
+        int blockSize = ceil((double) _size / omp_get_num_threads());
+        int id = omp_get_thread_num();
+        int st = id * blockSize;
+        int en = min(((id+1) * blockSize)-1, _size -1);
+        for (int i=st; i <= en; i++) {
             _elements[i] += v[i];
         }
+	}
+#else
+    	for (int i = 0; i < _size; i++) {
+            _elements[i] += v[i];
+        }
+#endif
         return *this;
     }
     LargeVec3Vector & operator -=(LargeVec3Vector const &v) {
-        for (int i = 0; i < _size; i++) {
+#ifdef ENABLE_OMP
+	#pragma omp parallel
+	{
+        int blockSize = ceil((double) _size / omp_get_num_threads());
+        int id = omp_get_thread_num();
+        int st = id * blockSize;
+        int en = min(((id+1) * blockSize)-1, _size -1);
+        for (int i=st; i <= en; i++) {
             _elements[i] -= v[i];
         }
-        return *this;
+	}
+#else
+    	for (int i = 0; i < _size; i++) {
+            _elements[i] -= v[i];
+        }
+#endif
+    	return *this;
     }
     LargeVec3Vector & operator *=(double v) {
-        for (int i = 0; i < _size; i++) {
+#ifdef ENABLE_OMP
+	#pragma omp parallel
+	{
+        int blockSize = ceil((double) _size / omp_get_num_threads());
+        int id = omp_get_thread_num();
+        int st = id * blockSize;
+        int en = min(((id+1) * blockSize)-1, _size -1);
+        for (int i=st; i <= en; i++) {
             _elements[i] *= v;
         }
-        return *this;
+	}
+#else
+    	for (int i = 0; i < _size; i++) {
+            _elements[i] *= v;
+        }
+#endif
+    	return *this;
     }
     LargeVec3Vector & operator /=(double v) {
-        for (int i = 0; i < _size; i++) {
+#ifdef ENABLE_OMP
+	#pragma omp parallel
+	{
+        int blockSize = ceil((double) _size / omp_get_num_threads());
+        int id = omp_get_thread_num();
+        int st = id * blockSize;
+        int en = min(((id+1) * blockSize)-1, _size -1);
+        for (int i=st; i <= en; i++) {
             _elements[i] /= v;
         }
-        return *this;
+	}
+#else
+    	for (int i = 0; i < _size; i++) {
+            _elements[i] /= v;
+        }
+#endif
+    	return *this;
     }
     double Dot(LargeVec3Vector const &v) {
         double ret = 0;
+#ifdef ENABLE_OMP
+	#pragma omp parallel
+	{
+        int blockSize = ceil((double) v.size() / omp_get_num_threads());
+        int id = omp_get_thread_num();
+        int st = id * blockSize;
+        int en = min(((id+1) * blockSize)-1, v.size() -1);
+        double myret = 0; //temporary return value per thread.
+        for (int i=st; i <= en; i++) {
+        	myret += (_elements[i] * v._elements[i]);
+        }
+        ret += myret;
+	}
+#else
         for (int i = 0; i < _size; i++) {
             ret += (_elements[i] * v._elements[i]);
         }
+#endif
         return ret;
     }
 
