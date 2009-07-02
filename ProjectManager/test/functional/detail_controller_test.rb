8063da29a26fad3344f6a0fc9bebc6ee7aa114bd
njoubert macbook <njoubert macbook@997b1fe7-5dcb-cc6a-e049-b7da040a1fbe>
2009-07-02 20:02:09 +0000

    faulty commit.
    git-svn-id: svn+ssh://njoubert.com/var/svn@992 997b1fe7-5dcb-cc6a-e049-b7da040a1fbe

--- ProjectManager/test/functional/detail_controller_test.rb
+++
@@ -1,8 +0,0 @@
-require 'test_helper'
-
-class DetailControllerTest < ActionController::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
-  end
-end
--- ProjectManager/test/unit/helpers/detail_helper_test.rb
+++
@@ -1,4 +0,0 @@
-require 'test_helper'
-
-class DetailHelperTest < ActionView::TestCase
-end
