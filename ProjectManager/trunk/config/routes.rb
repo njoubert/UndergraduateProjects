180304b19839787dd459a3f09c32d916198c0ec2
njoubert <njoubert@997b1fe7-5dcb-cc6a-e049-b7da040a1fbe>
2009-07-03 02:36:55 +0000

    Projects created
    git-svn-id: svn+ssh://njoubert.com/var/svn@1004 997b1fe7-5dcb-cc6a-e049-b7da040a1fbe

--- ProjectManager/trunk/config/routes.rb
+++ ProjectManager/trunk/config/routes.rb
@@ -1,4 +1,6 @@
 ActionController::Routing::Routes.draw do |map|
+  map.resources :projects
+
   # The priority is based upon order of creation: first created -> highest priority.
 
   # Sample of regular route:
@@ -40,4 +42,6 @@ ActionController::Routing::Routes.draw do |map|
   # consider removing the them or commenting them out if you're using named routes and resources.
   map.connect ':controller/:action/:id'
   map.connect ':controller/:action/:id.:format'
+  
+  map.root :controller => "projects"
 end
