1c26663441e01fe2396c9d27ce7a86103868ce4a
njoubert <njoubert@997b1fe7-5dcb-cc6a-e049-b7da040a1fbe>
2009-11-09 07:06:01 +0000

    Ignoring dev data
    git-svn-id: svn+ssh://njoubert.com/var/svn@1017 997b1fe7-5dcb-cc6a-e049-b7da040a1fbe

--- BurbleServer/trunk/db/migrate/20091108220329_add_people_data.rb
+++ BurbleServer/trunk/db/migrate/20091108220329_add_people_data.rb
@@ -2,13 +2,13 @@ require 'active_record/fixtures'
 
 class AddPeopleData < ActiveRecord::Migration
   def self.up
-    down
+    #down
     
-    dir = File.join(File.dirname(__FILE__), 'dev_data')
-    Fixtures.create_fixtures(dir, "people")
+    #dir = File.join(File.dirname(__FILE__), 'dev_data')
+    #Fixtures.create_fixtures(dir, "people")
   end
 
   def self.down
-    Person.delete_all
+    #Person.delete_all
   end
 end
