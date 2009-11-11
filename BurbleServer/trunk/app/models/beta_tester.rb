2ed13520889190da536b7863baa269bf4f0e5461
njoubert <njoubert@997b1fe7-5dcb-cc6a-e049-b7da040a1fbe>
2009-11-11 21:56:52 +0000

    More validation so we dont get spammed
    git-svn-id: svn+ssh://njoubert.com/var/svn@1037 997b1fe7-5dcb-cc6a-e049-b7da040a1fbe

--- BurbleServer/trunk/app/models/beta_tester.rb
+++ BurbleServer/trunk/app/models/beta_tester.rb
@@ -1,4 +1,6 @@
 class BetaTester < ActiveRecord::Base
   validates_presence_of :email
   validates_uniqueness_of :email
+  validates_inclusion_of :device, :in => ["iPod Touch", "iPhone"], :message => "should be iPod Touch or iPhone"
+  validates_format_of :email, :with => /^([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})$/i
 end
