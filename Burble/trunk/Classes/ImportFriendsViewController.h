be46418db50dcb579588723cfe7827efa4be7ccc
njoubert <njoubert@997b1fe7-5dcb-cc6a-e049-b7da040a1fbe>
2009-11-18 07:59:13 +0000

    new files for facebook import view
    git-svn-id: svn+ssh://njoubert.com/var/svn@1069 997b1fe7-5dcb-cc6a-e049-b7da040a1fbe

---
+++ Burble/trunk/Classes/ImportFriendsViewController.h
@@ -0,0 +1,18 @@
+//
+//  ImportFriendsViewController.h
+//  Test1
+//
+//  Created by Niels Joubert on 11/17/09.
+//  Copyright 2009 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface ImportFriendsViewController : UIViewController {
+
+}
+
+-(IBAction)importFriends;
+
+@end
