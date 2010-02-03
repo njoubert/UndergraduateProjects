7f965f507680d8afc94d4890800b45f1b01b77cd
njoubert macpro <njoubert macpro@997b1fe7-5dcb-cc6a-e049-b7da040a1fbe>
2010-02-03 03:51:07 +0000

    Using nothing but compare and set, both for atomic long and for return values of recursive functions, we blow it out of the water.
    git-svn-id: svn+ssh://njoubert.com/var/svn@1197 997b1fe7-5dcb-cc6a-e049-b7da040a1fbe

--- CS149_PA2/trunk/cs149/stm/STMTreap.java
+++ CS149_PA2/trunk/cs149/stm/STMTreap.java
@@ -1,12 +1,8 @@
 package cs149.stm;
 
-import cs149.stm.CoarseLockTreap.Node;
+import java.util.concurrent.atomic.AtomicLong;
 
 public class STMTreap implements IntSet {
-	
-	/**
-	 * Single Node in Treap
-	 */
     static class Node {
         final int key;
         final int priority;
@@ -25,10 +21,10 @@ public class STMTreap implements IntSet {
         }
     }
 
-    private long randState = 0;
+    private AtomicLong randState = new AtomicLong(0);
     private Node root;
 
-    @org.deuce.Atomic public boolean contains(final int key) {
+    public @org.deuce.Atomic boolean contains(final int key) {
         Node node = root;
         while (node != null) {
             if (key == node.key) {
@@ -39,77 +35,56 @@ public class STMTreap implements IntSet {
         return false;
     }
 
-    
-    @org.deuce.Atomic public void add(final int key) {
-    	//This is an issue. By setting the root to a value, 
-    	//we will abort all read instructions somewhere in it. 
-    	//Really we want to search down until we find where to modify, then modify.
-    	addImpl(root, null, key);
+    public @org.deuce.Atomic void add(final int key) {
+        Node temp = addImpl(root, key);
+        if (root != temp)
+        	root = temp;
     }
 
-    private void addImpl(final Node node, final Node parent, final int key) {
+    private Node addImpl(final Node node, final int key) {
         if (node == null) {
-            root = new Node(key, randPriority());
-        } else if (key == node.key) {
-            return;// no insert needed
-        } else if (key < node.key) {
-        	if (node.left == null) {
-        		node.left = new Node(key, randPriority());
-        	} else {
-	            addImpl(node.left, node, key);
-        	}
-        	if (node.left.priority > node.priority) {
-                rotateRight(node, parent);
+            return new Node(key, randPriority());
+        }
+        else if (key == node.key) {
+            // no insert needed
+            return node;
+        }
+        else if (key < node.key) {
+        	Node temp = addImpl(node.left, key);
+        	if (temp != node.left)
+        		node.left = temp;
+            if (node.left.priority > node.priority) {
+                return rotateRight(node);
             }
+            return node;
         }
         else {
-        	if (node.right == null)
-        		node.right = new Node(key, randPriority());
-        	else
-        		addImpl(node.right, node, key);
+            Node temp = addImpl(node.right, key);
+            if (temp != node.right)
+            	node.right = temp;
             if (node.right.priority > node.priority) {
-                rotateLeft(node, parent);
+                return rotateLeft(node);
             }
+            return node;
         }
     }
 
     private int randPriority() {
         // The constants in this 64-bit linear congruential random number
         // generator are from http://nuclear.llnl.gov/CNP/rng/rngman/node4.html
-        randState = randState * 2862933555777941757L + 3037000493L;
-        return (int)(randState >> 30);
+    	long state = randState.get();
+        while(!randState.compareAndSet(state, state * 2862933555777941757L + 3037000493L)) {
+        	state = randState.get();
+        }
+        return (int)((state * 2862933555777941757L + 3037000493L) >> 30);
     }
 
-    private void rotateRight(final Node node, final Node parent) {
+    private Node rotateRight(final Node node) {
         //       node                  nL
         //     /      \             /      \
         //    nL       z     ==>   x       node
         //  /   \                         /   \
         // x   nLR                      nLR   z
-        final Node nL = node.left;
-        node.left = nL.right;
-        nL.right = node;
-        if (parent == null)
-        	root = nL;
-        else if (parent.left.equals(node))
-        	parent.left = nL;
-        else
-        	parent.right = nL;
-    }
-
-    private void rotateLeft(final Node node, final Node parent) {
-        final Node nR = node.right;
-        node.right = nR.left;
-        nR.left = node;
-        if (parent == null)
-        	root = nR;
-        else if (parent.right.equals(node))
-        	parent.right = nR;
-        else
-        	parent.left = nR;
-    }
-    
-    private Node rotateRight(final Node node) {
         final Node nL = node.left;
         node.left = nL.right;
         nL.right = node;
@@ -123,52 +98,56 @@ public class STMTreap implements IntSet {
         return nR;
     }
 
-    @org.deuce.Atomic public void remove(final int key) {
-    	//Same problem as with add, this sets the root to something. We want to first search to where we should set a value, then start setting it.
-        removeImpl(root, null, key);
+    public @org.deuce.Atomic void remove(final int key) {
+        Node temp = removeImpl(root, key);
+        if (temp != root)
+        	root = temp;
     }
 
-    private Node removeImpl(final Node node, final Node parent, final int key) {
+    private Node removeImpl(final Node node, final int key) {
         if (node == null) {
-            root = new Node(key, randPriority());
+            // not present, nothing to do
+            return null;
         }
-        else if (key == node.key) { //We have found what we want to remove.
+        else if (key == node.key) {
             if (node.left == null) {
-            	if (parent == null)
-            		root = node.right;
-            	else if (parent.left.equals(node))
-            		parent.left = node.right;
-            	else
-            		parent.right = node.right;
+                // splice out this node
+                return node.right;
             }
             else if (node.right == null) {
-            	if (parent == null)
-            		root = node.left;
-            	else if (parent.left.equals(node))
-            		parent.left = node.left;
-            	else
-            		parent.right = node.left;
-            } else {
+                return node.left;
+            }
+            else {
                 // Two children, this is the hardest case.  We will pretend
                 // that node has -infinite priority, move it down, then retry
                 // the removal.
                 if (node.left.priority > node.right.priority) {
                     // node.left needs to end up on top
                     final Node top = rotateRight(node);
-                    top.right = removeImpl(top.right, key);
+                    Node temp = removeImpl(top.right, key);
+                    if (temp != top.right)
+                    	top.right = temp;
                     return top;
                 } else {
                     final Node top = rotateLeft(node);
-                    top.left = removeImpl(top.left, key);
+                    Node temp = removeImpl(top.left, key);
+                    if (temp != top.left)
+                    	top.left = temp;
                     return top;
                 }
             }
         }
         else if (key < node.key) {
-            removeImpl(node.left, node, key);
+        	Node temp = removeImpl(node.left, key);
+        	if (temp != node.left)
+        		node.left = temp;
+            return node;
         }
         else {
-            removeImpl(node.right, node, key);
+            Node temp = removeImpl(node.right, key);
+            if (temp != node.right)
+            	node.right = temp;
+            return node;
         }
     }
 }
