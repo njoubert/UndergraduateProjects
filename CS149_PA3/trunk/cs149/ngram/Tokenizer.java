e02d537d274d8fb37262901e1c931bd969fe4fcd
njoubert <njoubert@997b1fe7-5dcb-cc6a-e049-b7da040a1fbe>
2010-02-17 03:06:15 +0000

    initial import
    git-svn-id: svn+ssh://njoubert.com/var/svn@1199 997b1fe7-5dcb-cc6a-e049-b7da040a1fbe

---
+++ CS149_PA3/trunk/cs149/ngram/Tokenizer.java
@@ -0,0 +1,66 @@
+// Tokenizer
+package cs149.ngram;
+
+import java.util.Iterator;
+
+public class Tokenizer implements Iterator<String> {
+    private final String _src;
+    private int _pos;
+    private String _avail;
+
+    public Tokenizer(final String src) {
+        _src = src;
+        _pos = 0;
+        advance();
+    }
+
+    private static boolean isSentenceEnd(final char ch) {
+        return ch == '.' || ch == '!' || ch == '?';
+    }
+
+    private void advance() {
+        final int n = _src.length();
+
+        // skip non-alphanumeric and non-sentence-ending characters
+        char ch = '\0';
+        while (_pos < n) {
+            ch = _src.charAt(_pos);
+            if (isSentenceEnd(ch) || Character.isLetterOrDigit(_src.charAt(_pos))) {
+                break;
+            }
+            ++_pos;
+        }
+
+        // gobble alphanumeric characters or period, whichever we found first
+        if (isSentenceEnd(ch)) {
+            ++_pos;
+            while (_pos < n && isSentenceEnd(_src.charAt(_pos))) {
+                ++_pos;
+            }
+            _avail = "#";
+        } else {
+            final int begin = _pos;
+            while (_pos < n && Character.isLetterOrDigit(_src.charAt(_pos))) {
+                ++_pos;
+            }
+            _avail = (_pos == begin) ? null : _src.substring(begin, _pos).toLowerCase();
+        }
+    }
+
+    public boolean hasNext() {
+        return _avail != null;
+    }
+
+    public String next() {
+        if (!hasNext()) {
+            throw new IllegalStateException();
+        }
+        final String result = _avail;
+        advance();
+        return result;
+    }
+
+    public void remove() {
+        throw new UnsupportedOperationException();
+    }
+}
